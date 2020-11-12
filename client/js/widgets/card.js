502c0193516c00de71783f3f5e9b8f2f3cec8a1d
ArnoldSmith86 <arnold.smith.86@mail.ru>
2020-11-12 16:14:25 +0100

    don't recreate card faces every time the widget changes

--- client/js/widgets/card.js
+++ client/js/widgets/card.js
@@ -22,35 +22,40 @@ class Card extends Widget {
   receiveUpdate(object) {
     super.receiveUpdate(object);
     this.domElement.classList.add('card');
-    if(this.deck) {
+    if(this.deck && !this.domElement.innerHTML) {
       if(typeof this.enlarge == 'undefined' && this.deck.sourceObject.enlarge)
         this.enlarge = this.deck.sourceObject.enlarge;
 
       this.domElement.style.width = (this.width = this.deck.sourceObject.cardWidth || 103) + 'px';
       this.domElement.style.height = (this.height = this.deck.sourceObject.cardHeight || 160) + 'px';
 
-      this.domElement.innerHTML = '';
-      const face = this.deck.sourceObject.faceTemplates[this.sourceObject.activeFace || 0];
+      for(const face of this.deck.sourceObject.faceTemplates) {
+        const faceDiv = document.createElement('div');
 
-      const faceDiv = document.createElement('div');
+        faceDiv.style.border = face.includeBorder ? '1px black solid' : 'none';
+        faceDiv.style.borderRadius = face.includeRadius ? '8px' : '0';
 
-      faceDiv.style.border = face.includeBorder ? '1px black solid' : 'none';
-      faceDiv.style.borderRadius = face.includeRadius ? '8px' : '0';
-
-      for(const object of face.objects) {
-        const objectDiv = document.createElement('div');
-        const value = object.valueType == 'static' ? object.value : this.deck.sourceObject.cardTypes[this.sourceObject.cardType][object.value];
-        objectDiv.style.cssText = `left: ${object.x}px; top: ${object.y}px; width: ${object.w}px; height: ${object.h}px; font-size: ${object.fontSize}px; text-align: ${object.textAlign}`;
-        if(object.type == 'image') {
-          objectDiv.style.backgroundImage = `url(${value})`;
-          objectDiv.style.backgroundColor = object.color || 'white';
-        } else {
-          objectDiv.textContent = value;
-          objectDiv.style.color = object.color;
+        for(const object of face.objects) {
+          const objectDiv = document.createElement('div');
+          const value = object.valueType == 'static' ? object.value : this.deck.sourceObject.cardTypes[this.sourceObject.cardType][object.value];
+          objectDiv.style.cssText = `left: ${object.x}px; top: ${object.y}px; width: ${object.w}px; height: ${object.h}px; font-size: ${object.fontSize}px; text-align: ${object.textAlign}`;
+          if(object.type == 'image') {
+            objectDiv.style.backgroundImage = `url(${value})`;
+            objectDiv.style.backgroundColor = object.color || 'white';
+          } else {
+            objectDiv.textContent = value;
+            objectDiv.style.color = object.color;
+          }
+          faceDiv.appendChild(objectDiv);
         }
-        faceDiv.appendChild(objectDiv);
+        this.domElement.appendChild(faceDiv);
       }
-      this.domElement.appendChild(faceDiv);
+    }
+    for(let i=0; i<this.domElement.children.length; ++i) {
+      if(i == (this.sourceObject.activeFace || 0))
+        this.domElement.children[i].classList.add('active');
+      else
+        this.domElement.children[i].classList.remove('active');
     }
   }
 }
