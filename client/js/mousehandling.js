b300e5e0cc348ea91d85ba98aa6ba12fc70ac400
ArnoldSmith86 <arnold.smith.86@mail.ru>
2020-12-26 23:08:04 +0100

    changed click detection to be more fuzzy

--- client/js/mousehandling.js
+++ client/js/mousehandling.js
@@ -35,17 +35,21 @@ function inputHandler(name, e) {
       document.activeElement.blur();
       mouseStatus[target.id] = {
         status: 'initial',
+        start: new Date(),
         downCoords: coords
       };
     } else if(name == 'mouseup' || name == 'touchend') {
       batchStart();
-      if(mouseStatus[target.id].status == 'initial') {
+      const ms = mouseStatus[target.id];
+      const timeSinceStart = +new Date() - ms.start;
+      const pixelsMoved = ms.coords ? Math.abs(ms.coords[0] - ms.downCoords[0]) + Math.abs(ms.coords[1] - ms.downCoords[1]) : 0;
+      if(ms.status == 'initial' || timeSinceStart < 250 && pixelsMoved < 10) {
         if(edit)
           editClick(widgets.get(target.id));
         else if(widgets.get(target.id).click)
           widgets.get(target.id).click();
-      } else if(mouseStatus[target.id].widget.p(edit ? 'movableInEdit' : 'movable')) {
-        mouseStatus[target.id].widget.moveEnd();
+      } else if(ms.widget.p(edit ? 'movableInEdit' : 'movable')) {
+        ms.widget.moveEnd();
       }
       delete mouseStatus[target.id];
       batchEnd();
@@ -54,14 +58,15 @@ function inputHandler(name, e) {
       if(mouseStatus[target.id].status == 'initial') {
         const targetRect = target.getBoundingClientRect();
         const downCoords = mouseStatus[target.id].downCoords;
-        mouseStatus[target.id] = {
+        Object.assign(mouseStatus[target.id], {
           status: 'moving',
           offset: [ downCoords[0] - (targetRect.left + targetRect.width/2), downCoords[1] - (targetRect.top + targetRect.height/2) ],
           widget: widgets.get(target.id)
-        };
+        });
         if(mouseStatus[target.id].widget.p(edit ? 'movableInEdit' : 'movable'))
           mouseStatus[target.id].widget.moveStart();
       }
+      mouseStatus[target.id].coords = coords;
       const x = Math.floor((coords[0] - roomRectangle.left - mouseStatus[target.id].offset[0]) / scale);
       const y = Math.floor((coords[1] - roomRectangle.top  - mouseStatus[target.id].offset[1]) / scale);
       if(mouseStatus[target.id].widget.p(edit ? 'movableInEdit' : 'movable'))
