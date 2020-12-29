539f92dd12d3d2f7d54e522431825a822335cc68
Ben Hochstedler <ben.hochstedler@med.ge.com>
2020-12-29 06:23:09 -0500

    Change node config to ESM module

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/en/configuration.html
  */
 
-module.exports = {
+export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
