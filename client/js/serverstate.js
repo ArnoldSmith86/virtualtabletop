c8cca3037681ac18956a63c68ab6f3d78200a96e
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-01-02 22:36:04 +0100

    fixed widget removal

--- client/js/serverstate.js
+++ client/js/serverstate.js
@@ -88,7 +88,7 @@ function batchEnd() {
 function receiveDelta(delta) {
   // the order of widget changes is not necessarily correct and in order to avoid cyclic children, this first moves affected widgets to the top level
   for(const widgetID in delta.s)
-    if(delta.s[widgetID].parent !== undefined && widgets.has(widgetID))
+    if(delta.s[widgetID] && delta.s[widgetID].parent !== undefined && widgets.has(widgetID))
       $('#topSurface').appendChild(widgets.get(widgetID).domElement);
 
   for(const widgetID in delta.s) {
