634c97f8c3b11d0c153d26232f9fac99b31f884f
RaphaelAlvez <73476656+RaphaelAlvez@users.noreply.github.com>
2021-01-05 04:33:32 -0300

    chess icon

--- server/minify.mjs
+++ server/minify.mjs
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import os from 'os';
 import path from 'path';
 import zlib from 'zlib';
 
@@ -9,73 +10,73 @@ import uglifyES from '@node-minify/uglify-es';
 import htmlMinifier from '@node-minify/html-minifier';
 
 export default function minifyRoom() {
-  let roomHTML = fs.readFileSync(path.resolve() + '/client/room.html', {encoding:'utf8'});
+    let roomHTML = fs.readFileSync(path.resolve() + '/client/room.html', { encoding: 'utf8' });
 
-  return new Promise((resolve, reject) => {
-    minify({
-      compressor: cleanCSS,
-      input: [
-        'client/css/layout.css',
+    return new Promise((resolve, reject) => {
+        minify({
+            compressor: cleanCSS,
+            input: [
+                'client/css/layout.css',
 
-        'client/css/editmode.css',
+                'client/css/editmode.css',
 
-        'client/css/overlays/players.css',
-        'client/css/overlays/states.css',
+                'client/css/overlays/players.css',
+                'client/css/overlays/states.css',
 
-        'client/css/widgets/basicwidget.css',
-        'client/css/widgets/button.css',
-        'client/css/widgets/card.css',
-        'client/css/widgets/classes.css',
-        'client/css/widgets/deck.css',
-        'client/css/widgets/holder.css',
-        'client/css/widgets/label.css',
-        'client/css/widgets/pile.css',
-        'client/css/widgets/spinner.css'
-      ],
-      output: '/tmp/out.css'
-    }).then(function(min) {
-      roomHTML = roomHTML.replace(/ \{\{CSS\}\} /, min);
-      return minify({
-        compressor: process.env.NOCOMPRESS ? noCompress : uglifyES,
-        input: [
-          'client/js/domhelpers.js',
-          'client/js/connection.js',
-          'client/js/serverstate.js',
-          'client/js/editmode.js',
-          'client/js/geometry.js',
-          'client/js/mousehandling.js',
-          'client/js/statemanaged.js',
-          'client/js/widgets/widget.js',
+                'client/css/widgets/basicwidget.css',
+                'client/css/widgets/button.css',
+                'client/css/widgets/card.css',
+                'client/css/widgets/classes.css',
+                'client/css/widgets/deck.css',
+                'client/css/widgets/holder.css',
+                'client/css/widgets/label.css',
+                'client/css/widgets/pile.css',
+                'client/css/widgets/spinner.css'
+            ],
+            output: os.tmpdir() + '/out.css'
+        }).then(function(min) {
+            roomHTML = roomHTML.replace(/ \{\{CSS\}\} /, min);
+            return minify({
+                compressor: process.env.NOCOMPRESS ? noCompress : uglifyES,
+                input: [
+                    'client/js/domhelpers.js',
+                    'client/js/connection.js',
+                    'client/js/serverstate.js',
+                    'client/js/editmode.js',
+                    'client/js/geometry.js',
+                    'client/js/mousehandling.js',
+                    'client/js/statemanaged.js',
+                    'client/js/widgets/widget.js',
 
-          'client/js/widgets/basicwidget.js',
-          'client/js/widgets/button.js',
-          'client/js/widgets/card.js',
-          'client/js/widgets/deck.js',
-          'client/js/widgets/holder.js',
-          'client/js/widgets/label.js',
-          'client/js/widgets/pile.js',
-          'client/js/widgets/spinner.js',
+                    'client/js/widgets/basicwidget.js',
+                    'client/js/widgets/button.js',
+                    'client/js/widgets/card.js',
+                    'client/js/widgets/deck.js',
+                    'client/js/widgets/holder.js',
+                    'client/js/widgets/label.js',
+                    'client/js/widgets/pile.js',
+                    'client/js/widgets/spinner.js',
 
-          'client/js/overlays/players.js',
-          'client/js/overlays/states.js',
+                    'client/js/overlays/players.js',
+                    'client/js/overlays/states.js',
 
-          'client/js/main.js'
-        ],
-        output: '/tmp/out.js'
-      });
-    }).then(function(min) {
-      return minify({
-        compressor: htmlMinifier,
-        content: roomHTML.replace(/ \{\{JS\}\} /, min),
-        options: {
-          conservativeCollapse: true
-        }
-      })
-    }).then(function(min) {
-      roomHTML = min;
-      zlib.gzip(min, (err, gzipped) => {
-        resolve({ min, gzipped });
-      });
+                    'client/js/main.js'
+                ],
+                output: os.tmpdir() + '/out.js'
+            });
+        }).then(function(min) {
+            return minify({
+                compressor: htmlMinifier,
+                content: roomHTML.replace(/ \{\{JS\}\} /, min),
+                options: {
+                    conservativeCollapse: true
+                }
+            })
+        }).then(function(min) {
+            roomHTML = min;
+            zlib.gzip(min, (err, gzipped) => {
+                resolve({ min, gzipped });
+            });
+        });
     });
-  });
 }
