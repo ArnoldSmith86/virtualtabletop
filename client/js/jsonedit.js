c5d84817d048e07ae8746d85c52aef3f4ba1499f
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-01-29 16:53:57 +0100

    added button operations to new JSON editor

--- client/js/jsonedit.js
+++ client/js/jsonedit.js
@@ -25,7 +25,6 @@ const jeCommands = [
     name: 'toggle zoom out',
     forceKey: 'Z',
     call: function() {
-      console.log("yes");
       jeZoomOut = !jeZoomOut;
       if(jeZoomOut) {
         $('body').classList.add('jeZoomOut');
@@ -37,13 +36,10 @@ const jeCommands = [
   },
   {
     name: _=>`remove property ${jeContext && jeContext[jeContext.length-1]}`,
-    forceKey: 'd',
+    forceKey: 'D',
     context: ' ↦ (?=[^"]+$)',
     call: function() {
-      let pointer = jeStateNow;
-      for(const key of jeContext.slice(1))
-        if(typeof pointer[key] == 'object')
-          pointer = pointer[key];
+      let pointer = jeGetValue();
       delete pointer[jeContext[jeContext.length-1]];
 
       const oldStart = $('#jeText').selectionStart;
@@ -53,7 +49,7 @@ const jeCommands = [
       $('#jeText').selectionEnd   = oldEnd;
     },
     show: function() {
-      return jeStateNow && jeStateNow[jeContext && jeContext[jeContext.length-1]] !== undefined;
+      return jeStateNow && jeGetValue()[jeContext[jeContext.length-1]] !== undefined;
     }
   },
   {
@@ -70,8 +66,39 @@ const jeCommands = [
         $('#updateWidgetJSON').click();
       }
     }
+  },
+  {
+    name: _=>`change ${jeContext && jeContext[4]} to applyVariables`,
+    context: '^button.*\\) ↦ [a-zA-Z]+',
+    call: function() {
+      const operation = jeGetValue(jeContext.slice(1, 3));
+      delete operation[jeContext[4]];
+      if(!operation.applyVariables)
+        operation.applyVariables = [];
+      operation.applyVariables.push({ parameter: jeContext[4], variable: '###SELECT ME###' });
+      jeSetAndSelect('');
+    },
+    show: function() {
+      return jeContext && jeContext[4] != 'applyVariables';
+    }
   }
 ];
+
+function jeAddButtonOperationCommands(command, defaults) {
+  defaults.skip = false;
+  for(const property in defaults) {
+    jeCommands.push({
+      name: property,
+      context: `^button.* ↦ \\(${command}\\) ↦ `,
+      call: function() {
+        jeInsert(jeContext.slice(1, 3), property, defaults[property]);
+      },
+      show: function() {
+        return jeStateNow && jeGetValue()[property] === undefined;
+      }
+    });
+  }
+}
 
 function jeAddCommands() {
   jeAddWidgetPropertyCommands(new BasicWidget());
@@ -82,6 +109,23 @@ function jeAddCommands() {
   jeAddWidgetPropertyCommands(new Label());
   jeAddWidgetPropertyCommands(new Pile());
   jeAddWidgetPropertyCommands(new Spinner());
+
+  jeAddButtonOperationCommands('CLICK', { collection: 'DEFAULT', count: 1 });
+  jeAddButtonOperationCommands('COMPUTE', { operation: '+', operand1: 1, operand2: 1, operand3: 1, variable: 'COMPUTE' });
+  jeAddButtonOperationCommands('COUNT', { collection: 'DEFAULT', variable: 'COUNT' });
+  jeAddButtonOperationCommands('FLIP', { count: 0, face: null });
+  jeAddButtonOperationCommands('GET', { variable: 'id', collection: 'DEFAULT', property: 'id', aggregation: 'first' });
+  // INPUT is missing
+  jeAddButtonOperationCommands('LABEL', { value: 0, mode: 'set', label: null });
+  jeAddButtonOperationCommands('MOVE', { count: 1, face: null, from: null, to: null });
+  jeAddButtonOperationCommands('MOVEXY', { count: 1, face: null, from: null, x: 0, y: 0 });
+  jeAddButtonOperationCommands('RANDOM', { min: 1, max: 10, variable: 'RANDOM' });
+  jeAddButtonOperationCommands('RECALL', { owned: true, holder: null });
+  jeAddButtonOperationCommands('ROTATE', { count: 1, angle: 90, mode: 'add', holder: null });
+  jeAddButtonOperationCommands('SELECT', { type: 'all', property: 'parent', relation: '==', value: null, max: 999999, collection: 'DEFAULT', mode: 'add', source: 'all' });
+  jeAddButtonOperationCommands('SET', { collection: 'DEFAULT', property: 'parent', relation: '=', value: null });
+  jeAddButtonOperationCommands('SORT', { key: 'value', reverse: false, holder: null });
+  jeAddButtonOperationCommands('SHUFFLE', { holder: null });
 }
 
 function jeAddWidgetPropertyCommands(object) {
@@ -126,6 +170,14 @@ function jeGetContext() {
   const select = v.substr(s, e-s);
   const line = v.substr(0, s).split('\n').pop();
 
+  try {
+    jeStateNow = JSON.parse(v);
+    jeJSONerror = null;
+  } catch(e) {
+    jeStateNow = null;
+    jeJSONerror = e;
+  }
+
   let keys = [ t ];
   for(const line of v.substr(0, s).split('\n')) {
     const m = line.match(/^( +)(["{])([^"]*)/);
@@ -135,64 +187,95 @@ function jeGetContext() {
       keys = keys.slice(0, depth+1);
     }
   }
+  try {
+    if(keys[1] == 'clickRoutine' && typeof keys[2] == 'number' && !jeJSONerror)
+      keys.splice(3, 0, '(' + jeStateNow.clickRoutine[keys[2]].func + ')');
+  } catch(e) {}
+
   if(select)
     keys.push(`"${select}"`);
 
-  try {
-    jeStateNow = JSON.parse(v);
-    jeJSONerror = null;
-  } catch(e) {
-    jeStateNow = null;
-    jeJSONerror = e;
-  }
-
   jeContext = keys;
 
-  const context = keys.join(' ↦ ');
-  const usedKeys = { c: 1, x: 1, v: 1, w: 1, n: 1, t: 1, q: 1, j: 1, z: 1 };
-  let commandText = '';
-  for(const command of jeCommands) {
-    delete command.currentKey;
-    if(context.match(new RegExp(command.context)) && (!command.show || command.show())) {
-      const name = typeof command.name == 'function' ? command.name() : command.name;
-      if(command.forceKey)
-        command.currentKey = command.forceKey;
-      for(const key of name.split(''))
-        if(!command.currentKey && !usedKeys[key.toLowerCase()])
-          command.currentKey = key.toLowerCase();
-      for(const key of 'abcdefghijklmnopqrstuvwxyz1234567890'.split(''))
-        if(!command.currentKey && !usedKeys[key])
-          command.currentKey = key;
-      usedKeys[command.currentKey] = true;
-      commandText += `Ctrl-${command.currentKey}: ${name}\n`;
-    }
-  }
-  if(jeJSONerror)
-    commandText += `\n${String(jeJSONerror)}\n`;
-  $('#jeCommands').textContent = commandText;
+  jeShowCommands();
 
   return jeContext;
 }
 
+function jeGetValue(context, all) {
+  let pointer = jeStateNow;
+  for(const key of context || jeContext)
+    if(all && typeof pointer[key] !== undefined || typeof pointer[key] == 'object')
+      pointer = pointer[key];
+  return pointer
+}
+
 function jeInsert(context, key, value) {
   if(!jeJSONerror) {
-    let pointer = jeStateNow;
-    for(const key of context)
-      if(typeof pointer[key] == 'object')
-        pointer = pointer[key];
-    pointer[key] = "###SELECT ME###";
-    let jsonString = JSON.stringify(jeStateNow, null, '  ');
-    const startIndex = jsonString.indexOf("\"###SELECT ME###\"");
-    let length = jsonString.length-pointer[key].length-2;
-
-    pointer[key] = value;
-    jsonString = JSON.stringify(jeStateNow, null, '  ');
-
-    $('#jeText').value = jsonString;
-    $('#jeText').selectionStart = startIndex;
-    $('#jeText').selectionEnd = startIndex+jsonString.length-length;
-    $('#jeText').focus();
+    let pointer = jeGetValue(context);
+    pointer[key] = '###SELECT ME###';
+    jeSetAndSelect(value);
   }
+}
+
+function jeSetAndSelect(replaceBy) {
+  let jsonString = JSON.stringify(jeStateNow, null, '  ');
+  const startIndex = jsonString.indexOf('"###SELECT ME###"');
+  let length = jsonString.length-17;
+
+  jsonString = JSON.stringify(jeStateNow, null, '  ').replace(/"###SELECT ME###"/, JSON.stringify(replaceBy));
+
+  $('#jeText').value = jsonString;
+  $('#jeText').selectionStart = startIndex + (typeof replaceBy == 'string' ? 1 : 0);
+  $('#jeText').selectionEnd = startIndex+jsonString.length-length - (typeof replaceBy == 'string' ? 1 : 0);
+  $('#jeText').focus();
+}
+
+function jeShowCommands() {
+  const activeCommands = {};
+
+  const context = jeContext.join(' ↦ ');
+  for(const command of jeCommands) {
+    delete command.currentKey;
+    const contextMatch = context.match(new RegExp(command.context));
+    if(contextMatch && (!command.show || command.show())) {
+      if(activeCommands[contextMatch[0]] === undefined)
+        activeCommands[contextMatch[0]] = [];
+      activeCommands[contextMatch[0]].push(command);
+    }
+  }
+
+  const usedKeys = { c: 1, x: 1, v: 1, w: 1, n: 1, t: 1, q: 1, j: 1, z: 1 };
+  let commandText = '';
+
+  const sortByName = function(a, b) {
+    const nameA = typeof a.name == 'function' ? a.name() : a.name;
+    const nameB = typeof b.name == 'function' ? b.name() : b.name;
+    return nameA.localeCompare(nameB);
+  }
+
+  for(const contextMatch of (Object.keys(activeCommands).sort((a,b)=>b.length-a.length))) {
+    commandText += `\n  <b>${contextMatch}</b>\n`;
+    for(const command of activeCommands[contextMatch].sort(sortByName)) {
+      if(context.match(new RegExp(command.context)) && (!command.show || command.show())) {
+        const name = typeof command.name == 'function' ? command.name() : command.name;
+        if(command.forceKey && !usedKeys[command.forceKey])
+          command.currentKey = command.forceKey;
+        for(const key of name.split(''))
+          if(key != ' ' && !command.currentKey && !usedKeys[key.toLowerCase()])
+            command.currentKey = key.toLowerCase();
+        for(const key of 'abcdefghijklmnopqrstuvwxyz1234567890'.split(''))
+          if(!command.currentKey && !usedKeys[key])
+            command.currentKey = key;
+        usedKeys[command.currentKey] = true;
+        commandText += `Ctrl-${command.currentKey}: ${name.replace(command.currentKey, '<b>' + command.currentKey + '</b>')}\n`;
+      }
+    }
+  }
+  commandText += `\n${context}\n`;
+  if(jeJSONerror)
+    commandText += `\n${String(jeJSONerror)}\n`;
+  $('#jeCommands').innerHTML = commandText;
 }
 
 window.addEventListener('mousemove', function(e) {
