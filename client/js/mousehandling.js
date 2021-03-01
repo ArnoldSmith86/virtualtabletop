79871944d6a6982f1df2ca8771ae230939150aa1
RaphaelAlvez <73476656+RaphaelAlvez@users.noreply.github.com>
2021-03-01 01:59:45 -0300

    Revert "Fixes click off bug"

--- client/js/mousehandling.js
+++ client/js/mousehandling.js
@@ -19,11 +19,7 @@ function inputHandler(name, e) {
     if(!edit || !e.target.parentNode || !e.target.parentNode.className.match(/label/))
       return;
   e.preventDefault();
-  
-  if(name == 'mousedown' || name == 'touchstart') {
-    window.getSelection().collapseToEnd();
-    document.activeElement.blur();
-  }
+
   let target = e.target;
   while(target && (!target.id || !widgets.has(target.id)))
     target = target.parentNode;
@@ -36,6 +32,7 @@ function inputHandler(name, e) {
 
   if(target && target.id) {
     if(name == 'mousedown' || name == 'touchstart') {
+      document.activeElement.blur();
       mouseStatus[target.id] = {
         status: 'initial',
         start: new Date(),
