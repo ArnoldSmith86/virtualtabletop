09ba55ec633989e5112a0656d26b69849d9dc656
RaphaelAlvez <73476656+RaphaelAlvez@users.noreply.github.com>
2021-03-10 16:10:43 -0300

    extend image, svgReplace and color to button

--- client/js/widgets/button.js
+++ client/js/widgets/button.js
@@ -58,6 +58,33 @@ export class Button extends Widget {
     }
   }
 
+  css() {
+    let css = super.css();
+
+    if(this.p('color'))
+      css += '; --color:' + this.p('color');
+    if(this.p('image'))
+      css += '; background-image: url("' + this.getImage() + '")';
+
+    return css;
+  }
+
+  cssProperties() {
+    const p = super.cssProperties();
+    p.push('image', 'color', 'svgReplaces');
+    return p;
+  }
+
+  getImage() {
+    if(!Object.keys(this.p('svgReplaces')).length)
+      return this.p('image');
+
+    const replaces = {};
+    for(const key in this.p('svgReplaces'))
+      replaces[key] = this.p(this.p('svgReplaces')[key]);
+    return getSVG(this.p('image'), replaces, _=>this.domElement.style.cssText = this.css());
+  }
+
   async click() {
     function setDefaults(routine, defaults) {
       for(const key in defaults)
@@ -600,3 +627,4 @@ export class Button extends Widget {
     return widgetFilter(w=>this.toA(ids).indexOf(w.p('id')) != -1).forEach(callback);
   }
 }
+
