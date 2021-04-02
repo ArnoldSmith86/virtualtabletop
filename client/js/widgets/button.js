2a9e73e1182b74b751735808cfbf7d4e429a29e8
Lars Kobbe <35028592+larkob@users.noreply.github.com>
2021-04-02 21:06:56 +0200

    keep = in COMPUTE

--- client/js/widgets/button.js
+++ client/js/widgets/button.js
@@ -174,7 +174,7 @@ export class Button extends Widget {
       function compute(o, v, x, y, z) {
         try {
           switch(o) {
-          case 'set':  v = y;    break;
+          case '=':  v = y;    break;
           case '+':  v = x + y;  break;
           case '-':  v = x - y;  break;
           case '*':  v = x * y;  break;
@@ -505,9 +505,10 @@ export class Button extends Widget {
 
       if(a.func == 'SET') {
         setDefaults(a, { collection: 'DEFAULT', property: 'parent', relation: '=', value: null });
-        if(a.relation == '==')
+        if(a.relation == '==') {
           problems.push(`Warning: Relation == interpreted as =`);
-        a.relation = (a.relation != '==' && a.relation != '=') ? a.relation : 'set';
+          a.relation = '=';
+        }
         if((a.property == 'parent' || a.property == 'deck') && a.value !== null && !widgets.has(a.value)) {
           problems.push(`Tried setting ${a.property} to ${a.value} which doesn't exist.`);
         } else if(isValidCollection(a.collection)) {
