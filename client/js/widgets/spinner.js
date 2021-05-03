af0991103bfa65791144c7a2a3df7a54606dd5c7
RaphaelAlvez <73476656+RaphaelAlvez@users.noreply.github.com>
2021-05-03 18:01:50 -0300

    changes to properties names

--- client/js/widgets/spinner.js
+++ client/js/widgets/spinner.js
@@ -15,8 +15,8 @@ class Spinner extends Widget {
       backgroundCSS: '',
       spinnerCSS: '',
       valueCSS: '',
-      styleOptionsColor:'#000000',
-      styleOptionsLines:'#d2d2d2'
+      textColor:'#000000',
+      lineColor :'#d2d2d2'
 
     });
   }
@@ -64,7 +64,7 @@ class Spinner extends Widget {
       line.setAttribute('y1', 50);
       line.setAttribute('x2', 50 + Math.sin(0.5*Math.PI+2*Math.PI/options.length*i)*50);
       line.setAttribute('y2', 50 + Math.cos(0.5*Math.PI+2*Math.PI/options.length*i)*50);
-      line.setAttribute('stroke', this.p('styleOptionsLines'));
+      line.setAttribute('stroke', this.p('lineColor'));
 
       const text = document.createElementNS(ns, 'text');
       text.setAttribute('x', 50 + Math.sin(0.5*Math.PI-2*Math.PI/options.length*(+i+0.5))*38);
@@ -72,7 +72,7 @@ class Spinner extends Widget {
       text.setAttribute('dominant-baseline', 'central');
       text.setAttribute('text-anchor', 'middle');
       text.setAttribute('font-size', 20-options.length/2);
-      text.setAttribute('fill', this.p('styleOptionsColor'));
+      text.setAttribute('fill', this.p('textColor'));
 
       text.textContent = options[i];
 
