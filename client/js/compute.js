9158913a2bea7504e569fe9525e46c9fbe80c371
Lars Kobbe <github@larskobbe.de>
2021-05-20 09:41:20 +0000

    fix SET

--- client/js/compute.js
+++ client/js/compute.js
@@ -3,7 +3,7 @@ const compute_ops = [
       name: '=',
       desc: 'sets the variable to the value of x',
       sample: 'var a = ${x}',
-      call: function(v, x) { return v = x }
+      call: function(v, x, y) { return v = x = y } // y for compatibility with SET
     },
     {
       name: '+',
