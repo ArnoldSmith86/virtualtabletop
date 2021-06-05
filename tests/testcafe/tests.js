8fb18b86d69eca33ff3a95f8ee124ae99b91c841
Lars Kobbe <lars.kobbe@netdimensions.com>
2021-06-05 23:12:14 +0200

    fixed typo

--- tests/testcafe/tests.js
+++ tests/testcafe/tests.js
@@ -218,7 +218,7 @@ test('Dynamic expressions', async t => {
     ['var m = ${b} match \'a.c\'', 'm', shouldBe(['abc'])],
     ['var b = remove 1 2', 'b', shouldBe('a')],
     ['var test = \'b\' in ${b}', 'test', shouldBe(false)],
-    ['var test = ${b} includes \'b\'', 'test', shouldBe(false)],
+    ['var test = ${b} includes \'b\'', 'test', shouldBe(false)]
   ];
 
   ops.forEach((o)=>{
