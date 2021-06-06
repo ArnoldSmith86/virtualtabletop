778df9d67059a30f3fa596ea453002b348f219a0
David Robarts <robartsd@yahoo.com>
2021-06-06 11:11:27 -0700

    added pileParent pseudo property
    Creates a read-only pseudo property named pileParent.  pileParent returns the widget's parent property unless the parent widget is a pile, then it recursively looks up the chain of parents for a non-pile ancestor widget.

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -1023,6 +1023,18 @@ export class Widget extends StateManaged {
     return { variable: variables.result, collection: collections.result || [] };
   }
 
+  get(property) {
+    if(property == 'pileParent') {
+      if(widgets.get(this.get('parent')).get('type')=='pile') {
+        return widgets.get(this.get('parent')).get('pileParent');
+      } else {
+        return this.get('parent');
+      }
+    } else {
+      return super.get(property);
+    }
+  }
+  
   hideEnlarged() {
     $('#enlarged').classList.add('hidden');
   }
