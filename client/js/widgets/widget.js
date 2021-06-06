f01f99e4d6116dbb527edbff3cf7bd58e0285ce4
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-06-06 22:39:40 +0200

    fixed it a bit

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -1000,58 +1000,29 @@ export class Widget extends StateManaged {
       }
 
       if(a.func == 'TURN') {
-        setDefaults(a, { value: 0, mode: 'inc', collection: 'all' });
-          if([ 'set', 'dec', 'inc' ].indexOf(a.mode) == -1)
-            problems.push(`Warning: Mode ${a.mode} interpreted as set.`);
-          let c = (a.collection == 'all' ? Array.from(widgets.values()) : collections[a.collection]).filter(function(w) {
-            w.get('type')=='seat'
-          });
+        setDefaults(a, { value: 1, mode: 'inc', collection: 'all' });
+        if([ 'set', 'dec', 'inc' ].indexOf(a.mode) == -1)
+          problems.push(`Warning: Mode ${a.mode} interpreted as set.`);
+        const c = (a.collection == 'all' ? Array.from(widgets.values()) : collections[a.collection]).filter(function(w) {
+          return w.get('type')=='seat'
+        });
 
-          var index = []
-          for (w in c) {
-            if (index.indexOf(w.get('index'))==-1)
-              index.push(w.get('index'));
-          }
-          index.sort();
+        const index = [...new Set(c.map(w=>w.get('index')))].sort();
 
-          var turn = this.get('turn') || 0;
+        let turn = this.get('turn') || 0;
 
-          //loop so it goes for the n next valid index
-          for (var i = 0; i < a.value; i++) {
-            //this checks the next valid index
-            if (a.mode=='inc'){
-              if (turn >= index[index.length-1]){
-                turn = index[0]
-              } else {
-                  for (i in index){
-                    if (i>turn){
-                      turn = i;
-                      break
-                    }
-                  }
-                }
-            } else if (a.mode=='dec'){
-              //this checks the previous valid index
-                if (turn >= index[0]){
-                  turn = index[index.length-1]
-                } else {
-                    for (i in index){
-                      if (i<turn){
-                        turn = i;
-                      } else {break}
-                    }
-                  }
-            }
-          }
-      if (a.mode=='set'){
-        turn = a.value
-      }
+        console.log(c, index, turn);
 
-      for (w in c) {
-        w.set('turn', turn)
-      }
+        if(a.mode=='set') {
+          turn = a.value;
+        } else {
+          turn = index[(index.indexOf(turn) + a.value * (a.mode == 'inc' ? 1 : -1)) % index.length];
+        }
 
-      this.set('turn',turn);
+        for(const w of c)
+          await w.set('turn', turn);
+
+        this.set('turn', turn);
       }
 
       if(this.get('debug')) {
