b5c6c1f2853f1d8e4a95a728fef28592a7f7be80
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2021-06-18 11:06:19 -0500

    Update label.js
    Added functions to toggle between applying HTML and not applying HTML.  Added if statement to applyDeltaToDOM to apply upon changes.

--- client/js/widgets/label.js
+++ client/js/widgets/label.js
@@ -1,5 +1,37 @@
 import { Widget } from './widget.js';
 
+export async function unsetHTML(w) {
+  document.getElementById(w.id).innerHTML = null;
+	w.input = document.createElement('textarea');
+	 
+	w.addDefaults({
+    height: 20,
+    movable: false,
+    layer: -2,
+    typeClasses: 'widget label',
+
+    text: '',
+    editable: false,
+    twoRowBottomAlign: false
+    });
+  w.domElement.appendChild(w.input);
+	w.input.value = w.get('text'); 
+  w.input.addEventListener('keyup', e=>w.setText(e.target.value));  
+  }
+ 
+  export async function setHTML(w) {
+    if(w.get('html') == true) {
+      var HTMLtext = w.get('text')
+	    var HTMLtext = HTMLtext.toString()
+	    .replace(/\*\*(.*?)\*\*/gim, '<b>$1</b>')
+	    .replace(/\*(.*?)\*/gim, '<i>$1</i>')
+      .replace(/\-\-(.*?)\-\-/gim, '<sub>$1</sub>')
+   	  .replace(/\^\^(.*?)\^\^/gim, '<sup>$1</sup>')
+      .replace(/<[^A-Za-z0-9]*a +href(.*?)>/im, '<a href=https://github.com/ArnoldSmith86/virtualtabletop/wiki/Widget-Properties#label>')
+	    .replace(/<[^A-Za-z0-9]*iframe(.*?)>/im, '<iframe src=https://virtualtabletop.io/Tutorials> height="500" width="500"')
+      document.getElementById(w.id).innerHTML = HTMLtext;}
+    }
+
 export class Label extends Widget {
   constructor(id) {
     super(id);
@@ -35,6 +67,12 @@ export class Label extends Widget {
         this.input.style.height = `${this.get('height')}px`;
         this.input.style.paddingTop = 'unset';
       }
+    }
+    if(delta.html !== undefined) {
+      if(delta.html)
+        setHTML(this);
+      else
+        unsetHTML(this);
     }
     if(delta.editable !== undefined) {
       if(delta.editable)
