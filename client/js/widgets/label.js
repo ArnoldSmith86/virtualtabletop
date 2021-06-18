c24c8766f3e3bc3cee4c6cd44dcb380a500052b6
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2021-06-18 15:08:07 -0500

    Update label.js
    Mostly getting rid of unneeded stuff.

--- client/js/widgets/label.js
+++ client/js/widgets/label.js
@@ -1,37 +1,24 @@
 import { Widget } from './widget.js';
 
-export async function unsetHTML(w) {
-  document.getElementById(w.id).innerHTML = null;
-	w.input = document.createElement('textarea');
-	 
-	w.addDefaults({
-    height: 20,
-    movable: false,
-    layer: -2,
-    typeClasses: 'widget label',
-
-    text: '',
-    editable: false,
-    twoRowBottomAlign: false
-    });
-  w.domElement.appendChild(w.input);
-	w.input.value = w.get('text'); 
-  w.input.addEventListener('keyup', e=>w.setText(e.target.value));  
+async function unsetHTML(w) {
+  w.domElement.innerHTML = null;
+	w.domElement.appendChild(w.input);
+	w.input.value = w.get('text');  
   }
  
-  export async function setHTML(w) {
-    if(w.get('html') == true) {
-      var HTMLtext = w.get('text')
-	    var HTMLtext = HTMLtext.toString()
-	    .replace(/<[^>]+?cript(.*?)>/gim, '')
-	    .replace(/<[^>]+?\((.*?)\)(.*?)>/gim, '')
-	    .replace(/\*\*(.*?)\*\*/gim, '<b>$1</b>')
-	    .replace(/\*(.*?)\*/gim, '<i>$1</i>')
-      .replace(/\-\-(.*?)\-\-/gim, '<sub>$1</sub>')
-   	  .replace(/\^\^(.*?)\^\^/gim, '<sup>$1</sup>')
-      .replace(/<[^A-Za-z0-9]*a +href(.*?)>/im, '<a href=https://github.com/ArnoldSmith86/virtualtabletop/wiki/Widget-Properties#label>')
-	    .replace(/<[^A-Za-z0-9]*iframe(.*?)>/im, '<iframe src=https://virtualtabletop.io/Tutorials> height="500" width="500"')
-      document.getElementById(w.id).innerHTML = HTMLtext;}
+async function setHTML(w) {
+  if(w.get('html') == true) {
+    var HTMLtext = w.get('text')
+	  var HTMLtext = HTMLtext.toString()
+	  .replace(/<[^>]+?cript(.*?)>/gim, '')
+	  .replace(/<[^>]+?\((.*?)\)(.*?)>/gim, '')
+	  .replace(/\*\*(.*?)\*\*/gim, '<b>$1</b>')
+	  .replace(/\*(.*?)\*/gim, '<i>$1</i>')
+    .replace(/\-\-(.*?)\-\-/gim, '<sub>$1</sub>')
+   	.replace(/\^\^(.*?)\^\^/gim, '<sup>$1</sup>')
+    .replace(/<[^A-Za-z0-9]*a +href(.*?)>/im, '<a href=https://github.com/ArnoldSmith86/virtualtabletop/wiki/Widget-Properties#label>')
+	  .replace(/<[^A-Za-z0-9]*iframe(.*?)>/im, '<iframe src=https://virtualtabletop.io/Tutorials> height="500" width="500"')
+    document.getElementById(w.id).innerHTML = HTMLtext;}
     }
 
 export class Label extends Widget {
