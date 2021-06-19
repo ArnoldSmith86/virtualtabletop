17433d9584486f48c949a82ae96fe43b6d4f0940
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2021-06-19 16:26:04 -0500

    Text area now not destroyed
    Text area not destroyed anymore.  Child div added.  Child div now given parent height. Toggles between displaying text area and div.  Div now contenteditable when "readonly" set to false (not working though).

--- client/js/widgets/label.js
+++ client/js/widgets/label.js
@@ -3,6 +3,9 @@ import { Widget } from './widget.js';
 export class Label extends Widget {
   constructor(id) {
     super(id);
+  	this.divinput = document.createElement('div');
+  	this.domElement.appendChild(this.divinput);
+  	this.divinput.addEventListener('keyup', e=>this.setText(e.target.value)); // may delete
     this.input = document.createElement('textarea');
 
     this.addDefaults({
@@ -34,6 +37,8 @@ export class Label extends Widget {
       } else {
         this.input.style.height = `${this.get('height')}px`;
         this.input.style.paddingTop = 'unset';
+     		this.divinput.style.height = `${this.get('height')}px`;
+        this.divinput.style.paddingTop = 'unset';
       }
     }
 	if(delta.html  !== undefined|| delta.text !== undefined) {
@@ -50,18 +55,21 @@ export class Label extends Widget {
   	    .replace(/\^\^(.*?)\^\^/gim, '<sup>$1</sup>')
         .replace(/<[^A-Za-z0-9]*a +href(.*?)>/im, '<a href=https://github.com/ArnoldSmith86/virtualtabletop/wiki/Widget-Properties#label>')
   	    .replace(/<[^A-Za-z0-9]*iframe(.*?)>/im, '<iframe src=https://virtualtabletop.io/Tutorials> height="500" width="500"')
-        document.getElementById(this.id).innerHTML = HTMLtext;
+    		this.divinput.innerHTML = HTMLtext;   
+  	    this.input.style.display = "none";
+	    	this.divinput.style.display = "block";           
         } else {
-            this.domElement.innerHTML = null; 
-            this.domElement.appendChild(this.input);
-            this.input.value = this.get('text');
+	          this.input.style.display = "block";
+		        this.divinput.style.display = "none";
               }	
 	  }
     if(delta.editable !== undefined) {
       if(delta.editable)
         this.input.removeAttribute("readonly");
+   	    this.divinput.contentEditable = true;
       else
         this.input.setAttribute("readonly", !delta.editable);
+   	    this.divinput.contentEditable = false;
     }
   }
 }
