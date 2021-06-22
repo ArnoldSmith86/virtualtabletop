c1d537985f619cab859e2a5f6124c85b70904510
David Robarts <robartsd@yahoo.com>
2021-06-22 15:42:50 -0700

    adjust bottom align to any size

--- client/js/widgets/label.js
+++ client/js/widgets/label.js
@@ -25,14 +25,19 @@ export class Label extends Widget {
     if(delta.text !== undefined || delta.twoRowBottomAlign !== undefined) {
       this.input.value = delta.text;
       if(this.get('twoRowBottomAlign')) {
-        this.input.style.minHeight = '20px';
-        this.input.style.paddingTop = '';
-        if(this.input.scrollHeight == 20)
-          this.input.style.paddingTop = '20px';
-        else
+        this.input.style.height = '20px';
+        this.input.style.minHeight = 'unset';
+        this.input.style.paddingTop = '0';
+        const contentHeight = this.input.scrollHeight;
+        if(contentHeight < this.get('height')) {
+          this.input.style.paddingTop = `${this.get('height')-contentHeight}px`;
+          this.input.style.height = 'auto';
+          this.input.style.minHeight = `${contentHeight}px`;
+        } else {
           this.input.style.minHeight = '100%';
+        }
       } else {
-        this.input.style.minHeight = `${this.get('height')}px`;
+        this.input.style.minHeight = '100%';
         this.input.style.paddingTop = 'unset';
       }
     }
