514e37837c976eb43eef6086971e5ca8f94ab45a
ArnoldSmith86 <73538315+ArnoldSmith86@users.noreply.github.com>
2021-06-26 00:45:39 +0200

    fixed undefined collections after `FOREACH` (#598)

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -622,10 +622,18 @@ export class Widget extends StateManaged {
             collections[add] = addCollections[add];
           }
           await this.evaluateRoutine(a.loopRoutine, variables, collections, (depth || 0) + 1, true);
-          for(const add in addVariables)
-            variables[add] = variableBackups[add];
-          for(const add in addCollections)
-            collections[add] = collectionBackups[add];
+          for(const add in addVariables) {
+            if(variableBackups[add] !== undefined)
+              variables[add] = variableBackups[add];
+            else
+              delete variables[add];
+          }
+          for(const add in addCollections) {
+            if(collectionBackups[add] !== undefined)
+              collections[add] = collectionBackups[add];
+            else
+              delete collections[add];
+          }
         }
         if(a.in) {
           for(const key in a.in)
