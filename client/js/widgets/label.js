c21df481f5164a291da6581ff73043ac4c96dd2e
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-07-10 15:56:20 +0200

    STAGING: HTML label property/setting (#582)

--- client/js/widgets/label.js
+++ client/js/widgets/label.js
@@ -3,8 +3,15 @@ import { Widget } from './widget.js';
 export class Label extends Widget {
   constructor(id) {
     super(id);
+    this.divinput = document.createElement('div');
+    this.divinput.className = "labeldiv";
+    this.domElement.appendChild(this.divinput);
+    this.divinput.addEventListener('focusout' , e => this.setFromDiv (e, false))
+    this.divinput.addEventListener('keydown' , e => {
+      if(e.shiftKey && e.keyCode == 13)
+        this.setFromDiv (e, true);
+    })
     this.input = document.createElement('textarea');
-
     this.addDefaults({
       height: 20,
       movable: false,
@@ -13,6 +20,7 @@ export class Label extends Widget {
 
       text: '',
       editable: false,
+      html: false,
       twoRowBottomAlign: false
     });
 
@@ -21,6 +29,21 @@ export class Label extends Widget {
       if(this.get('editable') && e.target.value != this.get('text'))
         this.setText(e.target.value)
     });
+  }
+
+  setFromDiv(e, toEnd){
+    e.preventDefault();
+    const t = e.target.innerHTML.trim()
+    .replace(/&lt;/gimu,'<')
+    .replace(/&gt;/gimu, '>')
+    .replace(/<span style="color: ([^>]+)?">/gimu, '<c: $1>')
+    .replace(/<span style="margin:auto; display:table">/gimu, '<center>')
+    .replace(/<span style="font-size: ([^>]+)?">/gimu, '<size: $1>')
+    this.setText(t + " ");
+    if(toEnd) {
+      document.execCommand('selectAll', false, null);
+      document.getSelection().collapseToEnd();
+    }
   }
 
   applyDeltaToDOM(delta) {
@@ -36,13 +59,36 @@ export class Label extends Widget {
       } else {
         this.input.style.paddingTop = '0';
         this.input.style.minHeight = '100%';
+        this.divinput.style.height = `${this.get('height')}px`;
+        this.divinput.style.paddingTop = 'unset';
       }
     }
+  if(delta.html  !== undefined|| delta.text !== undefined) {
+    if(this.get('html') == true) {
+      var HTMLtext = this.get('text').toString()
+      .replace(/=/gimu, '&#61')
+      .replace(/<c: ([^>]+)?>/gim, '<span style="color: $1">')
+      .replace(/<center>/gim, '<span style="margin:auto; display:table">')
+      .replace(/<size: ([^>]+)?>/gim, '<span style="font-size: $1">')
+      .replace(/<\/c>|<\/center>|<\/size>/gim, '</span>')
+      .replace(/\*\*(.*?)\*\*/gim, '<b>$1</b>')
+      .replace(/\*(.*?)\*/gim, '<i>$1</i>')
+      .replace(/\-\-(.*?)\-\-/gim, '<sub>$1</sub>')
+      .replace(/\^\^(.*?)\^\^/gim, '<sup>$1</sup>')
+      this.divinput.innerHTML = HTMLtext;
+      this.input.style.display = "none";
+      this.divinput.style.display = "inline-block";
+    } else {
+      this.input.style.display = "block";
+      this.divinput.style.display = "none";
+    }
+  }
     if(delta.editable !== undefined) {
       if(delta.editable)
         this.input.removeAttribute("readonly");
       else
         this.input.setAttribute("readonly", !delta.editable);
+      this.divinput.contentEditable = delta.editable;
     }
   }
 }
