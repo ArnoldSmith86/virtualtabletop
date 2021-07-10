645e09093d5690517ef59e119c61bf08a1f45a84
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-07-10 16:04:17 +0200

    STAGING: added virtual_ancestor pseudo property (#531)

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -1131,6 +1131,18 @@ export class Widget extends StateManaged {
     return { variable: variables.result, collection: collections.result || [] };
   }
 
+  get(property) {
+    if(property == '_ancestor') {
+      if(widgets.has(this.get('parent')) && widgets.get(this.get('parent')).get('type')=='pile') {
+        return widgets.get(this.get('parent')).get('_ancestor');
+      } else {
+        return this.get('parent');
+      }
+    } else {
+      return super.get(property);
+    }
+  }
+  
   hideEnlarged() {
     $('#enlarged').classList.add('hidden');
   }
