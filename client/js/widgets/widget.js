fc2db8109ba5437e2b1bfb50a7ef9e657b20db45
David Robarts <robartsd@yahoo.com>
2021-07-11 12:01:55 -0700

    prevent setting _ancestor

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -921,6 +921,8 @@ export class Widget extends StateManaged {
         }
         if((a.property == 'parent' || a.property == 'deck') && a.value !== null && !widgets.has(a.value)) {
           problems.push(`Tried setting ${a.property} to ${a.value} which doesn't exist.`);
+        } else if (a.property == "_ancestor") {
+          problems.push(`Tried setting read-only property ${a.property}.`;
         } else if (a.property == 'id' && isValidCollection(a.collection)) {
           for(const oldWidget of collections[a.collection]) {
             const oldState = JSON.stringify(oldWidget.state);
