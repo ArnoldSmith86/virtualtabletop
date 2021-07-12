fa3eb2a18e55832803160c76c3ed751325fdee29
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-07-12 12:19:53 +0200

    fixed `INPUT` type `select`

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -279,9 +279,7 @@ export class Widget extends StateManaged {
 
       if(field.type == 'checkbox') {
         result[field.variable] = document.getElementById(this.get('id') + ';' + field.variable).checked;
-      }
-
-      if(field.type == 'color' || field.type == 'number' || field.type == 'string') {
+      } else {
         result[field.variable] = document.getElementById(this.get('id') + ';' + field.variable).value;
       }
 
