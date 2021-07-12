d17e2a00eade4a76a78ee455d2bbc92c5dcfa6f0
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-07-12 12:30:21 +0200

    ignore type `text`

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -279,7 +279,7 @@ export class Widget extends StateManaged {
 
       if(field.type == 'checkbox') {
         result[field.variable] = document.getElementById(this.get('id') + ';' + field.variable).checked;
-      } else {
+      } else if(field.type != 'text') {
         result[field.variable] = document.getElementById(this.get('id') + ';' + field.variable).value;
       }
 
