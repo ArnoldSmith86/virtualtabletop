58d074ac31a717ac420d094459dd87e39f95ae8b
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2021-08-12 18:59:38 -0500

    Update tests.js

--- tests/testcafe/tests.js
+++ tests/testcafe/tests.js
@@ -50,24 +50,6 @@ function prepareClient() {
   document.querySelector('base').parentNode.removeChild(document.querySelector('base'));
 }
 
-function publicLibraryTest(game, variant, md5, tests) {
-  test.after(async t => {
-    await removeGame(t);
-    await t.expect(Selector('#statesOverlay').visible).ok();
-  })(`Public library: ${game} (variant ${variant})`, async t => {
-    await ClientFunction(prepareClient)();
-    await t
-      .pressKey('esc')
-      .click('#statesButton')
-      .click(Selector('td.name').withExactText(game).prevSibling().child())
-      .hover('.roomState')
-      .click(Selector('button.play').nth(variant));
-    await setName(t);
-    await tests(t);
-    await compareState(t, md5);
-  });
-}
-
 function hiddenTest(game, variant, md5, tests) {
   test.after(async t => {
     await removeGame(t);
@@ -79,6 +61,24 @@ function hiddenTest(game, variant, md5, tests) {
       .click('button.prettyButton.link:nth-child(2)')  
       .pressKey('http://212.47.248.129:3724/library/Test_Room.vtt#VTT')
       .pressKey('enter')
+      .hover('.roomState')
+      .click(Selector('button.play').nth(variant));
+    await setName(t);
+    await tests(t);
+    await compareState(t, md5);
+  });
+}
+
+function publicLibraryTest(game, variant, md5, tests) {
+  test.after(async t => {
+    await removeGame(t);
+    await t.expect(Selector('#statesOverlay').visible).ok();
+  })(`Public library: ${game} (variant ${variant})`, async t => {
+    await ClientFunction(prepareClient)();
+    await t
+      .pressKey('esc')
+      .click('#statesButton')
+      .click(Selector('td.name').withExactText(game).prevSibling().child())
       .hover('.roomState')
       .click(Selector('button.play').nth(variant));
     await setName(t);
@@ -355,5 +355,3 @@ publicLibraryButtons('Master Button',      0, 'eb19dffdb38641d5556e5fdb2c47c62b'
   'masterbutton', 'redbutton', 'orangebutton', 'yellowbutton', 'greenbutton', 'bluebutton', 'indigobutton',
   'violetbutton', 'fae4', 'vbx5'
 ]);
-
-
