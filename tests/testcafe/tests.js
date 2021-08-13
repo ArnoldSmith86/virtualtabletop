ea0452bd9a8d2dfe8302489cd89ae119e36a99e8
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2021-08-13 11:39:39 -0500

    Update tests.js

--- tests/testcafe/tests.js
+++ tests/testcafe/tests.js
@@ -60,7 +60,7 @@ function hiddenTest(game, variant, md5, tests) {
       .pressKey('esc')
       .click('#statesButton')
       .setNativeDialogHandler(() => {
-		  return '{ dependencies: { server }}/library/{ dependencies: { game }}.vtt#VTT'
+		  return `${server}/library/${game}.vtt#VTT`
             })
       .click('button.prettyButton.link:nth-child(2)')
       .pressKey('enter')
