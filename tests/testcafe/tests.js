1e5a7712f053798e26ed1829bf760906131f579f
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2021-08-14 00:57:22 -0500

    Update tests.js

--- tests/testcafe/tests.js
+++ tests/testcafe/tests.js
@@ -76,7 +76,7 @@ function hiddenLibraryButtons(game, variant, md5, buttons) {
     for(const b of buttons) {
       await t
       const clickSubmit = ClientFunction(() => {
-    document.getElementById(${b}).click().wait(20000);
+    document.getElementById('${b}').click().wait(20000);
 });
       await clickSubmit();
     }
