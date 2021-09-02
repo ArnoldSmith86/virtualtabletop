a0e51c94470d6ee586a94d8d2c024690f404dbce
Roger Lipsett <63379656+rogerl50@users.noreply.github.com>
2021-09-02 08:29:55 -0400

    Update widget.js
    Fix spacing in new code

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -1463,10 +1463,10 @@ export class Widget extends StateManaged {
       if(typeof w1.get(key) == 'number')
         return w1.get(key) - w2.get(key);
       else
-        if(w1.get(key) === null )
-          return w2.get(key) === null ?  0 : -1;
-      else
-        return w2.get(key) === null ? 1 : w1.get(key).localeCompare(w2.get(key), locales, options);
+        if(w1.get(key) === null)
+          return w2.get(key) === null ?  0 : -1
+        else
+          return w2.get(key) === null ? 1 : w1.get(key).localeCompare(w2.get(key), locales, options);
     });
     if(reverse)
       children = children.reverse();
