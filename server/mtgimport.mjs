9c80b4711a4a07600abe70b55351c5083b36873c
ArnoldSmith86 <arnold.smith.86@mail.ru>
2021-12-05 22:59:31 +0100

    use card names as cardType

--- server/mtgimport.mjs
+++ server/mtgimport.mjs
@@ -16,12 +16,13 @@ export default async function convertMTG(content) {
   for(const file in zip.files) {
     const content = await zip.files[file].async('string');
 
-    const data = JSON.parse(content.match(/DeckSampleHand" data-react-props="([^"]+)/)[1].replace(/&quot;/g, '"'));
+    const data = JSON.parse(content.match(/DeckSampleHand" data-react-props="([^"]+)/)[1].replace(/&quot;/g, '"').replace(/&#39;/g, "'"));
     const title = data.deck.title || content.match(/<h1.*?\n(.+)/)[1];
     decks[title] = { cards: [], sideboard: [] };
 
     for(const card of data.deck.entries) {
       decks[title][card.type == 'maindeck' ? 'cards' : 'sideboard'].push({
+        name: card.card,
         face: cardImage(card.card_database),
         back: card.card_database.related_cards.length ? cardImage(card.card_database.related_cards[0].card) : null,
         quantity: card.quantity
@@ -122,23 +123,21 @@ export default async function convertMTG(content) {
       width: 115,
       height: 160
     };
-    let i=1;
     for(const card of decks[deckTitle].cards) {
-      widgets[deckTitle].cardTypes[i] = {
+      widgets[deckTitle].cardTypes[card.name] = {
         face: card.face
       };
       if(card.back)
-        widgets[deckTitle].cardTypes[i].back = card.back;
+        widgets[deckTitle].cardTypes[card.name].back = card.back;
       for(let c=1; c<=card.quantity; ++c) {
-        widgets[deckTitle + '_' + i + '_' + c] = {
-          id: deckTitle + '_' + i + '_' + c,
+        widgets[deckTitle + '_' + card.name + '_' + c] = {
+          id: deckTitle + '_' + card.name + '_' + c,
           type: 'card',
           parent: deckTitle + '_pile',
           deck: deckTitle,
-          cardType: i
+          cardType: card.name
         };
       }
-      ++i;
     }
   }
 
