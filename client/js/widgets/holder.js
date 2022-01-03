23ad6d0201ad23697dc930daf81a388e3a505075
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2022-01-03 01:49:27 -0600

    Update holder.js

--- client/js/widgets/holder.js
+++ client/js/widgets/holder.js
@@ -57,10 +57,8 @@ class Holder extends Widget {
     }
     if(this.get('alignChildren') && (this.get('stackOffsetX') || this.get('stackOffsetY')))
       await this.receiveCard(null);
-    if(Array.isArray(this.get('leaveRoutine'))) {
+    if(Array.isArray(this.get('leaveRoutine')))
       await this.evaluateRoutine('leaveRoutine', {}, { child: [ card ] });
-      endRoutine = null; 
-    }
   }
 
   async onChildAdd(child, oldParentID) {
