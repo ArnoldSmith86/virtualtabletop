c7db02742786022e59c6f2cd4d27f47fb3a714ee
ArnoldSmith86 <arnold.smith.86@mail.ru>
2022-03-30 11:19:31 +0200

    using `Object.assign` instead

--- server/config.mjs
+++ server/config.mjs
@@ -6,22 +6,19 @@ class Config {
     if(!fs.existsSync(path.resolve() + '/config.json'))
       fs.copyFileSync(path.resolve() + '/config.template.json', path.resolve() + '/config.json');
 
-    this.config = JSON.parse(fs.readFileSync(path.resolve() + '/config.json'));
-    this.configFallback = JSON.parse(fs.readFileSync(path.resolve() + '/config.template.json'));
+    this.config = JSON.parse(fs.readFileSync(path.resolve() + '/config.template.json'));
+    this.config = Object.assign(this.config, JSON.parse(fs.readFileSync(path.resolve() + '/config.json')));
   }
 
   directory(index) {
-    if(this.get('directories')[index][0] == '/')
-      return this.get('directories')[index];
+    if(this.config.directories[index][0] == '/')
+      return this.config.directories[index];
     else
-      return path.resolve() + '/' + this.get('directories')[index];
+      return path.resolve() + '/' + this.config.directories[index];
   }
 
   get(index) {
-    if(this.config[index] !== undefined)
-      return this.config[index];
-    else
-      return this.configFallback[index];
+    return this.config[index];
   }
 }
 
