97438eac82fc39923792b5655199daf76ec7c087
David Robarts <368796+robartsd@users.noreply.github.com>
2022-04-17 18:58:40 +0000

    fix spinner crash when no options

--- client/js/widgets/spinner.js
+++ client/js/widgets/spinner.js
@@ -44,7 +44,8 @@ class Spinner extends Widget {
       const angle = this.get('angle') + Math.floor((2+Math.random())*360);
       const o = this.get('options');
       await this.set('angle', angle);
-      await this.set('value', o[Math.floor(angle/(360/o.length))%o.length]);
+      if(o.length)
+        await this.set('value', o[Math.floor(angle/(360/o.length))%o.length]);
     }
   }
 
