545ff06c9852b2626fe845a8621912db1bf141da
Roger Lipsett <63379656+rogerl50@users.noreply.github.com>
2022-07-16 16:33:39 -0400

    Minor fixes to new tree display functionality in JSON editor (#1262)
    * Do not redisplay tree within execution of a macro.
    
    Also minor fix to adjust tree pane size when macro is being defined.
    
    * Fix alignment of swap_vert button. (#1260)
    
    * Use setting-dependent up arrow/down arrow for F key ordering button icon.
    
    * show command options with empty editor
    
    Co-authored-by: ArnoldSmith86 <arnold.smith.86@mail.ru>

--- client/js/jsonedit.js
+++ client/js/jsonedit.js
@@ -10,6 +10,7 @@ let jeJSONerror = null;
 let jeCommandError = null;
 let jeCommandWithOptions = null;
 let jeFKeyOrderDescending = 1;
+let jeInMacroExecution = false;
 let jeContext = null;
 let jeSecondaryWidget = null;
 let jeDeltaIsOurs = false;
@@ -350,8 +351,10 @@ const jeCommands = [
         jeMode = 'macro';
         jeSetEditorContent(jeMacroPreset);
         jeColorize();
+        editPanel.style.setProperty('--treeHeight', "20%");
       } else {
         jeJSONerror = null;
+        jeInMacroExecution = true;
         try {
           const macro = new Function(`"use strict";return (function(w, v) {${jeGetEditorContent()}})`)();
           const variableState = {};
@@ -366,6 +369,8 @@ const jeCommands = [
         } catch(e) {
           jeJSONerror = e;
         }
+        jeDisplayTree();
+        jeInMacroExecution = false;
       }
       jeShowCommands();
     }
@@ -388,10 +393,11 @@ const jeCommands = [
   {
     id: 'je_reverseFkeys',
     name: 'Reverse order of F-key shortcuts',
-    icon: 'swap_vert',
+    icon:  _=>jeFKeyOrderDescending ==1 ? '[arrow_down]' : '[arrow_up]',
     forceKey: 'K',
     call: async function() {
       jeFKeyOrderDescending = -jeFKeyOrderDescending;
+      jeShowCommands();
     }
   },
   {
@@ -1360,7 +1366,7 @@ function jeUpdateTree(delta) {
   for(const id in delta) {
     if(typeof treeNodes[id] != 'undefined' && delta[id] != null && typeof delta[id].parent == 'undefined') {
       treeNodes[id].innerHTML = jeTreeGetWidgetHTML(widgets.get(id));
-    } else {
+    } else if(!jeInMacroExecution) {
       jeDisplayTree();
       if(jeDeltaIsOurs && delta[id] != null && typeof delta[id].id == 'string')
         jeCenterSelection();
@@ -1415,8 +1421,10 @@ function jeGetContext() {
     return jeContext;
   }
 
-  if(jeMode == 'empty')
+  if(jeMode == 'empty') {
+    jeShowCommands();
     return jeContext;
+  }
 
   if(jeMode == 'trace') {
     jeContext = [ 'Trace' ];
@@ -1954,8 +1962,8 @@ function jeEmpty() {
 
 const clickButton = async function(event) {
   await jeCallCommand(jeCommands.find(o => o.id == event.currentTarget.id));
+  jeGetContext();
   if(jeMode != 'macro' && jeMode != 'empty') {
-    jeGetContext();
     if((jeWidget || jeMode == 'multi') && !jeJSONerror)
       await jeApplyChanges();
     if (jeContext[0] == '###SELECT ME###')
