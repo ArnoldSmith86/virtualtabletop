fad13eee71f3b58f0e4468de9940b7fae875c55a
Roger Lipsett <63379656+rogerl50@users.noreply.github.com>
2022-08-06 08:45:07 -0400

    Move retrieval of widget type in jeGetContext (#1282)
    * Move retrieval of widget type in jeGetContext
    
    Fixes #584
    
    * Removed "t" to simplify code.

--- client/js/jsonedit.js
+++ client/js/jsonedit.js
@@ -1408,8 +1408,7 @@ function jeGetContext() {
   const s = Math.min(aO, fO);
   const e = Math.max(aO, fO);
   const v = jeGetEditorContent();
-  const t = jeStateNow && jeStateNow.type || 'basic';
-
+  
   const select = v.substr(s, Math.min(e-s, 100)).replace(/\n/g, '\\n');
   const line = v.split('\n')[v.substr(0, s).split('\n').length-1];
 
@@ -1453,7 +1452,7 @@ function jeGetContext() {
   }
 
   // go through all the lines up until the cursor and use the indentation to figure out the context
-  let keys = [ t ];
+  let keys = [ jeStateNow && jeStateNow.type || 'basic' ];
   for(const line of v.split('\n').slice(0, v.substr(0, s).split('\n').length)) {
     const m = line.match(/^( +)(["{])([^"]*)/);
     if(m) {
