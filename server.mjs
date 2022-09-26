15db89aa511bceb74a68668dd5fbbde6fb92218c
ArnoldSmith86 <arnold.smith.86@mail.ru>
2022-09-26 13:03:17 +0000

    allow content-type header

--- server.mjs
+++ server.mjs
@@ -139,6 +139,7 @@ MinifyRoom().then(function(result) {
   router.options('/state/:room', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.sendStatus(200);
   });
 
