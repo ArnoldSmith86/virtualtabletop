1268595cea308999f5b8fba65a277871eee90e76
rogerl50 <63379656+rogerl50@users.noreply.github.com>
2022-11-01 17:03:24 -0400

    Add horizontal player display.
    Rewrite table building code, rewrite css.

--- client/js/widgets/scoreboard.js
+++ client/js/widgets/scoreboard.js
@@ -1,3 +1,4 @@
+import { $, $a, removeFromDOM, asArray, escapeID } from '../domhelpers.js';
 import { Widget } from './widget.js';
 
 class ScoreBoard extends Widget {
@@ -9,6 +10,7 @@ class ScoreBoard extends Widget {
       layer: -1,
       typeClasses: 'widget scoreboard',
       usePlayerColors: true,
+      playersInColumns: true,
       rounds: null
     });
   }
@@ -23,86 +25,82 @@ class ScoreBoard extends Widget {
   applyInitiaDelta() {
   }
 
+  addRowToTable(parent, values, cellType = 'td') {
+    const tr = parent.insertRow();
+    const v = asArray(values);
+    tr.innerHTML = Array(values.length).fill(`<${cellType}></${cellType}>`).join('');
+    for (let i=0; i < values.length; i++)
+      $a(cellType, tr)[i].innerText = values[i];
+    return tr;
+  }
+
   tableCreate(players) {
-    const tbl = document.createElement('table');
-    const thead = document.createElement('thead');
-    const tbody = document.createElement('tbody');
-    const rounds = this.get('rounds');
-    const useRounds = Array.isArray(rounds);
 
-    tbl.appendChild(thead);
-    tbl.appendChild(tbody);
-
-    // Put player names in header
-    if(useRounds) {
-      const th = document.createElement('th');
-      th.textContent = 'Round';
-      thead.appendChild(th);
-    }
-    for (let i = 0; i < players.length; i++) {
-      const th = document.createElement('th');
-      th.textContent = players[i].get('player');
-      if(this.get('usePlayerColors')) {
-        th.style.backgroundColor = players[i].get('color');
-        th.style.color = 'white';
-      } else
-        th.style.color = 'black';
-      thead.appendChild(th);
-    }
-
-    // Compute transposed scores array
+    // Construct player scores array, remember maximum number of scores.
     let pScores = [];
     let totals = [];
+    const colors = []; // Array of player colors
     let numRounds = 0;
     for (let i=0; i < players.length; i++) {
       const score = players[i].get('score');
+      colors.push(players[i].get('color'));
+      if(Array.isArray(score) && score.length > numRounds)
+        numRounds = score.length;
+    }
+    for (let i=0; i < players.length; i++) {
+      const score = players[i].get('score');
       if(Array.isArray(score)) {
-        pScores[i] = score;
+        pScores[i] = [...score];
         totals[i] = score.reduce((partialSum, a) => partialSum + a, 0);
-        if(score.length > numRounds)
-          numRounds = score.length;
       } else {
         pScores[i] = [];
         totals[i] = Number.isInteger(score) ? parseInt(score) : 0;
       }
+      pScores[i] = pScores[i].concat(Array(numRounds).fill('')).slice(0,numRounds);
+      pScores[i].push(totals[i]);
+      pScores[i].unshift(players[i].get('player'));
     }
+    // Create round name headers
+    let rounds = this.get('rounds');
+    rounds = Array.isArray(rounds) ? [...rounds] : [];
+    rounds = rounds.concat(Array(numRounds).fill('')).slice(0,numRounds);
+    rounds.push('Totals');
+    rounds.unshift('Round');
 
-    // Fill in table rows
-    for (let j = 0; j < numRounds; j++) {
-      const tr = document.createElement('tr');
-      if(useRounds) {
-        const th = document.createElement('th');
-        th.textContent = rounds[j];
-        tr.appendChild(th)
+    let tbl = document.createElement('table');
+    if(this.get('playersInColumns')) {
+      const names = pScores.map(x => x[0]);
+      names.unshift('Round');
+      tbl.innerHTML += '<thead></thead>';
+      const tr = this.addRowToTable($('thead', tbl), names, 'th');
+      if(this.get('usePlayerColors'))
+        for (let i=0; i<players.length; i++ )
+          tr.cells[i+1].style.color = colors[i];
+      tbl.innerHTML += '<tbody></tbody>';
+      for( let i=1; i < pScores[0].length; i++ ) {
+        const pRow = pScores.map(x => x[i]);
+        pRow.unshift(rounds[i]);
+        const tr = this.addRowToTable($('tbody',tbl), pRow, 'td');
+        tr.cells[0].outerHTML = `<th>${tr.cells[0].innerHTML}</th>`;
+        if(i == pScores[0].length - 1)
+          for(let j=0; j <= pScores.length; j++)
+            tr.cells[j].classList.add('totalsLine');
       }
-      for (let i = 0; i < players.length; i++) {
-        const td = tr.insertCell();
-        td.textContent=pScores[i][j] || '';
+    } else {
+      this.addRowToTable(tbl, rounds, 'th');
+      for( let i=0; i < pScores.length; i++) {
+        const tr = this.addRowToTable(tbl, pScores[i], 'td');
+        tr.cells[0].outerHTML = `<th>${tr.cells[0].innerHTML}</th>`;
+        if(this.get('usePlayerColors'))
+            tr.cells[0].style.color = colors[i];
       }
-      tbody.appendChild(tr);
     }
 
-    // Append totals row
-    const tr = document.createElement('tr');
-    if(useRounds) {
-      const th = document.createElement('th');
-      th.textContent = 'Totals';
-      tr.appendChild(th)
-    }
-    for (let i=0; i < players.length; i++) {
-      const td = tr.insertCell();
-      td.textContent = totals[i];
-      td.classList.add('totalsLine');
-    }
-    tbody.appendChild(tr);
-
-    
-    // Put table into DOM
-    const here = this.domElement;
-    here.innerHTML = '';
-    here.appendChild(tbl);
-    here.style.width = (tbl.offsetWidth +20)+'px';
-    here.style.height = (tbl.offsetHeight + 20)+ 'px';
+    const myDom = this.domElement;
+    myDom.innerHTML = '';
+    myDom.appendChild(tbl);
+    myDom.style.width = (tbl.offsetWidth +20)+'px';
+    myDom.style.height = (tbl.offsetHeight + 20)+ 'px';
   }
   
 /*
