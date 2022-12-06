4a7bc5fb182f7c7b11783c60e068c3c40e075079
rogerl50 <63379656+rogerl50@users.noreply.github.com>
2022-12-06 15:15:03 -0500

    Clean up handling of teams.

--- client/js/widgets/scoreboard.js
+++ client/js/widgets/scoreboard.js
@@ -29,7 +29,9 @@ class ScoreBoard extends Widget {
   applyDeltaToDOM(delta) {
     if(delta) // Don't call super unless this is a real delta to the scoreboard rather than from a seat
       super.applyDeltaToDOM(delta);
-    this.tableCreate(this.getIncludedSeats())
+    const seats = this.getIncludedSeats();
+    if(seats !== null)
+      this.tableCreate(seats)
   }
   
   applyInitiaDelta() {
@@ -43,54 +45,51 @@ class ScoreBoard extends Widget {
     else {
       // First get total score for each relevant seat
       const totals = [];
-      let included = this.getIncludedSeats();
-      for (const seat of included) {
-        const score = seat.get(this.get('scoreProperty'));
-        const index = seat.get('index');
-        if(Array.isArray(score)) {
-          totals[index] = this.getTotal(score);
-        } else {
-          totals[index] = parseFloat(score);
+      let seats = this.getIncludedSeats();
+      if(Array.isArray(seats)) {// Getting seat totals
+        for (const seat of seats) {
+          const score = seat.get(this.get('scoreProperty'));
+          const index = seat.get('index');
+          totals[index] = this.getTotal(asArray(score).map(s => parseFloat(s)));
+          if(isNaN(totals[index]))
+            totals[index] = 0;
         }
-        if(isNaN(totals[index]))
-          totals[index] = 0;
-      }
-      // Now compute totals, taking into account whether we are using teams or not.
-      const accumItems = this.get('seats');
-      if(accumItems === null || Array.isArray(accumItems)) // Totals for individual seats
         return totals
-      else if(typeof accumItems == 'object') { // Totals for teams
+      } else if (typeof seats == 'object') { // Getting team totals
         const teamTotals = [null];
-        for (const team in accumItems) {
-          const seatsInTeam = widgetFilter(w => w.get('type') == 'seat' && (this.get('includeAllSeats') || w.get('player')) && accumItems[team].includes(w.get('id')));
-          const seatsScores = asArray(seatsInTeam.map(w => w.get(this.get('scoreProperty'))));
+        for (const team in seats) {
+          const seatsInTeam = widgetFilter(w => w.get('type') == 'seat' && seats[team].includes(w));
+          const seatsScores = asArray(seats[team].map(w => w.get(this.get('scoreProperty'))));
           const seatsTotals = seatsScores.map( s => this.getTotal(asArray(s)) );
           teamTotals.push(this.getTotal(seatsTotals));
         }
         return teamTotals;
-      } else // Neither, ignore request.
+      } else // Neither, ignore request
         return null;
     }
   }
 
+  // Return a modified 'seat' array or object including the seat widgets (not just the seat ids) to actually be used.
   getIncludedSeats() {
     const seats = this.get('seats');
     if(Array.isArray(seats) || seats === null) // Scoreboard just using seats
-       return widgetFilter(w => w.get('type') == 'seat' && (this.get('includeAllSeats') || w.get('player')) && (!seats || seats.includes(w.get('id'))))
-    else if(typeof seats == 'object') { // Scoreboard using teams
-      let included = [];
+       return [...widgetFilter(w => w.get('type') == 'seat' && (this.get('includeAllSeats') || w.get('player')) && (!seats || seats.includes(w.get('id'))))]
+    else if(seats !== null && typeof seats == 'object') { // Scoreboard using teams
+      const newSeats = {};
       for (const team in seats) {
-        if(!Array.isArray(seats[team]))
-          seats[team] = asArray(seats[team]);
-        included.push(... widgetFilter(w => w.get('type') == 'seat' && (this.get('includeAllSeats') || w.get('player')) && seats[team].includes(w.get('id'))));
+        newSeats[team] = asArray(seats[team]);
+        newSeats[team] = [... widgetFilter(w => w.get('type') == 'seat' && (this.get('includeAllSeats') || w.get('player')) && newSeats[team].includes(w.get('id')))];
       }
-      return included;
+      return newSeats;
     } else // 'seats' property is not array or object, ignore it.
       return null;
   }
 
   getTotal(array) {
-    return array.reduce((partialSum, a) => partialSum + a, 0)
+    let sum = array.reduce((partialSum, a) => partialSum + parseFloat(a), 0);
+    if(isNaN(sum))
+      sum = 0
+    return sum;
   }
 
   addRowToTable(parent, values, cellType = 'td') {
@@ -102,7 +101,7 @@ class ScoreBoard extends Widget {
     return tr;
   }
 
-  tableCreate(includedSeats) {
+  tableCreate(seats) {
     /* This routine creates the HTML table for display in the scoreboard. It is
      * complicated by the fact that the `seats` property can be either an array of
      * seat IDs or an object whose keys are team names and each of whose values is an
@@ -112,8 +111,6 @@ class ScoreBoard extends Widget {
      * pScores array to construct the HTML table.
      * There are lots of other things going on, to get the totals line, round names, etc
      * correct.
-     *
-     * includedSeats contains a list of all valid seats referenced in the `seats` parameter.
      */
     const showTotals = this.get('showTotals');
     const scoreProperty = this.get('scoreProperty');
@@ -122,22 +119,20 @@ class ScoreBoard extends Widget {
     const colors = []; // Array of player colors
 
     // Compute number of scoring rounds to show
-    let numRounds = 0; // Number of rounds to display
     let rounds = this.get('rounds'); // User-supplied round names
-    for (let i=0; i < includedSeats.length; i++) {
-      const score = includedSeats[i].get(scoreProperty);
-      if(Array.isArray(score) && score.length > numRounds)
-        numRounds = score.length;
-    }
+    let includedSeats = seats; // Array of all relevant seats
+    if(typeof(seats)=='object') // Array of all seats for team use
+      includedSeats = Object.keys(seats).reduce((union,key) => union.concat(seats[key]), []);
+
+    let numRounds = includedSeats.reduce((maximum, s) => Math.max(maximum, s.get(scoreProperty).length || 1), 0);
     if(this.get('showAllRounds') && Array.isArray(rounds))
       numRounds = Math.max(rounds.length, numRounds);
 
     let pScores = []; // Array of scores. pScores[i][0] is player name or team name, last is total (or last score if showTotals is false)
-    const displayItems = this.get('seats') || includedSeats; // One item per displayed row/column.
-    if(Array.isArray(displayItems)) { // Show individual seats
+    if(Array.isArray(seats)) { // Show individual seats
       // Fill player score array, totals array
-      for (let i=0; i < includedSeats.length; i++) {
-        const score = includedSeats[i].get(scoreProperty);
+      for (let i=0; i < seats.length; i++) {
+        const score = seats[i].get(scoreProperty);
         let total;
         if(Array.isArray(score)) {
           pScores[i] = score.map( s => isNaN(parseFloat(s)) ? 0 : parseFloat(s));
@@ -151,7 +146,7 @@ class ScoreBoard extends Widget {
         pScores[i] = pScores[i].concat(Array(numRounds).fill('')).slice(0,numRounds);
         if(showTotals)
           pScores[i].push(total);
-        pScores[i].unshift(includedSeats[i].get('player'));
+        pScores[i].unshift(seats[i].get('player'));
       }
 
       // Sort player scores as requested
@@ -170,17 +165,16 @@ class ScoreBoard extends Widget {
 
       // Get player colors if needed
       if(usePlayerColors)
-        for (let i=0; i < includedSeats.length; i++)
-          colors.push(includedSeats.filter(x=> x.get('player') == pScores[i][0])[0].get('color'));
-    } else if(displayItems && typeof displayItems == 'object') { // Display team scores
+        for (let i=0; i < seats.length; i++)
+          colors.push(seats.filter(x=> x.get('player') == pScores[i][0])[0].get('color'));
+    } else if(typeof seats == 'object') { // Display team scores
       usePlayerColors = false; // No predefined colors for teams
       let i = 0;
-      for (const team in displayItems) {
-        // Get array of (arrays of) seat scores, first removing invalid seat names from the team.
-        const thisTeam = displayItems[team].filter(w => widgets.has(w) && includedSeats.includes(widgets.get(w)));
-        const seatScores = thisTeam.map(w =>  asArray(widgets.get(w).get(scoreProperty)));
+      for (const team in seats) {
+        // Get array of (arrays of) seat scores.
+        const seatScores = seats[team].map(w =>  asArray(w.get(scoreProperty)));
 
-        // Make all score arrays for this the same length, then add element-by-element
+        // Make all score arrays for this the same length, then add them element-by-element
         const n = seatScores.reduce((max, xs) => Math.max(max, xs.length), 0);
         pScores[i] = Array(n).fill(0).map((_,i) => this.getTotal(seatScores.map(xs => xs[i] || 0)));
         pScores[i] = pScores[i].concat(Array(numRounds).fill('')).slice(0,numRounds);
@@ -248,7 +242,7 @@ class ScoreBoard extends Widget {
       if(showTotals)
           for(let c=0; c < numCols; c++)
             tbl.rows[numRows-1].cells[c].classList.add('totalsLine');
-    } else { // Players are in rows
+    } else { // Scores are in rows
       // Compute total number of rows and columns in table
       numCols = numRounds + 1 + (showTotals ? 1 : 0);
       numRows = pScores.length + 1;
