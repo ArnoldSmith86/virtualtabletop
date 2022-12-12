d1ed35166b5f2f2edf75f8f3da9976266048f52a
Robert Flack <flackr@gmail.com>
2022-12-12 18:49:49 -0500

    Fetch fonts.css from current path.

--- client/js/widgets/card.js
+++ client/js/widgets/card.js
@@ -154,7 +154,7 @@ class Card extends Widget {
             const css = object['css'];
             const extraStyles = typeof css == 'object' ? this.cssToStylesheet(css, true, object) : '';
             const html = `<!DOCTYPE html>\n` +
-                `<html><head><link rel="stylesheet" href="/fonts.css"><style>html,body {height: 100%; margin: 0;} html {font-size: 14px; font-family: 'Roboto', sans-serif;} body {overflow: hidden;}${extraStyles}` +
+                `<html><head><link rel="stylesheet" href="fonts.css"><style>html,body {height: 100%; margin: 0;} html {font-size: 14px; font-family: 'Roboto', sans-serif;} body {overflow: hidden;}${extraStyles}` +
                 `</style></head><body class="${object.classes || ""}">${content}</body></html>`;
             objectDiv.srcdoc = html;
           } else {
