42b7104017e20ac31d2902951cbdddedbb52ed83
MICHAEL W TAYLOR <pafbadc@gmail.com>
2022-12-19 23:13:57 -0600

    Still trying to fix error that shows up in TestCafe but not anywhere else

--- client/js/overlays/players.js
+++ client/js/overlays/players.js
@@ -5,6 +5,7 @@ let playerCursorsTimeout = {};
 let playerName = localStorage.getItem('playerName') || 'Guest' + Math.floor(Math.random()*1000);
 let playerColor = 'red';
 let activePlayers = [];
+let activeColors
 localStorage.setItem('playerName', playerName);
 
 export {
