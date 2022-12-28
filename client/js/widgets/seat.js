d764e141251dc8ba50439e13cc64a9439eb280d1
ArnoldSmith86 <arnold.smith.86@mail.ru>
2022-12-28 22:05:07 +0100

    moved scoreboard update to updateLinkedWidgets

--- client/js/widgets/seat.js
+++ client/js/widgets/seat.js
@@ -33,17 +33,13 @@ class Seat extends Widget {
       displayedText = displayedText.replaceAll('playerName',this.get('player'))
       setText(this.domElement, displayedText);
     }
-    const scoreboard = widgetFilter(w => w.get('type') == 'scoreboard');
-    for (const board of scoreboard)
-      board.applyDeltaToDOM(null);
-    
-    if(delta.player !== undefined)
-      this.updateLinkedWidgets();
+
+    this.updateLinkedWidgets(delta.player !== undefined);
   }
 
   applyInitialDelta(delta) {
     super.applyInitialDelta(delta);
-    this.updateLinkedWidgets();
+    this.updateLinkedWidgets(true);
   }
 
   classes() {
@@ -96,7 +92,12 @@ class Seat extends Widget {
     }
   }
 
-  updateLinkedWidgets() {
-    widgetFilter(w=>w.get('onlyVisibleForSeat') || w.get('linkedToSeat') || w.get('type') == 'seat').forEach(wc=>wc.updateOwner());
+  updateLinkedWidgets(playerChanged) {
+    const scoreboard = widgetFilter(w => w.get('type') == 'scoreboard');
+    for(const board of scoreboard)
+      board.applyDeltaToDOM(null);
+
+    if(playerChanged)
+      widgetFilter(w=>w.get('onlyVisibleForSeat') || w.get('linkedToSeat') || w.get('type') == 'seat').forEach(wc=>wc.updateOwner());
   }
 }
