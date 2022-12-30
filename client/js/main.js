d0aec7b1fc1dad2d2760676fffab1d0daee98c1a
ArnoldSmith86 <73538315+ArnoldSmith86@users.noreply.github.com>
2022-12-30 10:46:55 +0100

    game library overhaul (#1223)

--- client/js/main.js
+++ client/js/main.js
@@ -89,7 +89,6 @@ export function showOverlay(id, forced) {
   if(id) {
     const style = $(`#${id}`).style;
     style.display = !forced && style.display === 'flex' ? 'none' : 'flex';
-    $('#roomArea').className = style.display === 'flex' ? 'hasOverlay' : '';
     overlayActive = style.display === 'flex';
     if(forced)
       overlayActive = 'forced';
@@ -100,10 +99,16 @@ export function showOverlay(id, forced) {
     }
     toServer('mouse',{inactive:true})
   } else {
-    $('#roomArea').className = '';
     vmEditOverlay.selectedWidget = {};
     overlayActive = false;
   }
+}
+
+export function showStatesOverlay(id) {
+  showOverlay(id);
+  if(id == 'statesOverlay')
+    updateFilterOverflow();
+  $('#statesButton').dataset.overlay = id;
 }
 
 function checkURLproperties(connected) {
@@ -175,12 +180,18 @@ function setScale() {
   } else {
     scale = w/h < 1600/1000 ? w/1600 : h/1000;
   }
+  $('body').classList.remove('wideToolbar');
+  $('body').classList.remove('horizontalToolbar');
   if(w-scale*1600 + h-scale*1000 < 44) {
     $('body').classList.add('aspectTooGood');
     if(!$('body').className.match(/hiddenToolbar/))
       scale = (w-44)/1600;
   } else {
     $('body').classList.remove('aspectTooGood');
+    if(w - scale*1600 > 200)
+      $('body').classList.add('wideToolbar');
+    else if(w/h < 1600/1000)
+      $('body').classList.add('horizontalToolbar');
   }
   document.documentElement.style.setProperty('--scale', scale);
   roomRectangle = $('#roomArea').getBoundingClientRect();
@@ -201,7 +212,7 @@ export async function sortWidgets(collection, keys, reverse, locales, options, r
     r.push(false);
   const k = asArray(keys).map((key, i, k) => {
     const keyObj = {
-      key, 
+      key,
       locales,
       options
     };
@@ -301,7 +312,7 @@ function getSVG(url, replaces, callback) {
 
   if(!svgCache[url]) {
     svgCache[url] = [];
-    fetch(url.replace(/^\//, '')).then(r=>r.text()).then(t=>{
+    fetch(mapAssetURLs(url)).then(r=>r.text()).then(t=>{
       const callbacks = svgCache[url];
       svgCache[url] = t;
       for(const c of callbacks)
@@ -316,13 +327,43 @@ function getSVG(url, replaces, callback) {
 onLoad(function() {
   on('#pileOverlay', 'click', e=>e.target.id=='pileOverlay'&&showOverlay());
 
-  on('.toolbarButton', 'click', function(e) {
-    const overlay = e.target.dataset.overlay;
-    if(overlay)
-      showOverlay(overlay);
+  on('#toolbar > img', 'click', e=>$('#statesButton').click());
+
+  on('.toolbarTab', 'click', function(e) {
+    if(e.currentTarget.classList.contains('active')) {
+      if($('#stateDetailsOverlay.notEditing') && $('#stateDetailsOverlay.notEditing').style.display == 'flex')
+        showStatesOverlay('statesOverlay');
+      if(e.currentTarget == $('#activeGameButton') && $('#addOverlay').style.display == 'flex')
+        showOverlay();
+      e.stopImmediatePropagation();
+      return;
+    }
+    for(const tabButton of $a('.toolbarTab'))
+      toggleClass(tabButton, 'active', tabButton == e.currentTarget);
+
+    if(e.currentTarget == $('#editButton') || edit)
+      toggleEditMode();
   });
 
-  on('#muteButton', 'click', function(){
+  on('#addButton', 'click', function(e) {
+    if(!$a('#activeGameButton.active, #editButton.active').length)
+      $('#activeGameButton').click();
+  });
+
+  on('#activeGameButton', 'click', function() {
+    showOverlay();
+  });
+
+  on('.toolbarButton', 'click', function(e) {
+    const overlay = e.currentTarget.dataset.overlay;
+    if(overlay) {
+      showOverlay(overlay);
+      if(overlay == 'statesOverlay')
+        updateFilterOverflow();
+    }
+  });
+
+  on('#muteButton', 'click', function() {
     if(muted) {
       $('#volume').value = unmuteVol;
       $('#muteButton').classList.remove('muted');
@@ -376,7 +417,7 @@ onLoad(function() {
 
   if(Object.keys(config.betaServers).length) {
     for(const betaServerName in config.betaServers) {
-      const entry = domByTemplate('template-betaServerList-entry', 'tr');
+      const entry = domByTemplate('template-betaServerList-entry', {}, 'tr');
       $('button', entry).textContent = betaServerName;
       var thisstatus = config.betaServers[betaServerName].return ? 'check' : 'cancel';
       $('.return', entry).textContent = thisstatus;
@@ -385,7 +426,7 @@ onLoad(function() {
       $('#betaServerList').appendChild(entry);
     }
     on('#betaServerList button', 'click', function(e) {
-      toServer('setRedirect', e.target.textContent);
+      toServer('setRedirect', e.currentTarget.textContent);
     });
   } else {
     removeFromDOM($('#betaText'));
@@ -424,10 +465,8 @@ window.onresize = function(event) {
 
 window.onkeyup = function(event) {
   if(event.key == 'Escape') {
-    if(overlayActive)
-      showOverlay();
-    else if(edit)
-      toggleEditMode();
+    if(overlayActive || edit)
+      $('#activeGameButton').click();
     else if(jeEnabled && jeDebugViewing) {
       jeCallCommand(jeCommands.find(o => o.id == 'je_toggleDebug'));
       jeShowCommands();
