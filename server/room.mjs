f43457d5291e528a7652e782060290cb8c403e8d
ArnoldSmith86 <arnold.smith.86@mail.ru>
2023-01-01 01:44:39 +0100

    send meta update when setting state

--- server/room.mjs
+++ server/room.mjs
@@ -848,6 +848,7 @@ export default class Room {
       this.state = FileUpdater(this.state);
     this.state._meta = meta;
     this.broadcast('state', state);
+    this.sendMetaUpdate();
   }
 
   toggleStateStar(player, publicLibraryName) {
