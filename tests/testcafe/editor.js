df58787c10011abd79bef67b19d0c228229aebb3
96LawDawg <76912527+96LawDawg@users.noreply.github.com>
2023-01-05 11:13:15 -0600

    Fix TestCafe button problem for seat + counter

--- tests/testcafe/editor.js
+++ tests/testcafe/editor.js
@@ -47,6 +47,8 @@ test('Create game using edit mode', async t => {
     .click('#activeGameButton')
     .click('#addButton')
     .click('#addSeat')
+    .click('#addButton')
+    .click('#addSeatCounter')
     .click('#w_es5b');
 
   await compareState(t, '748d3bd6db5ea53524bd157ba41c8dd3');
