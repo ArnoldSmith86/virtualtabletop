035245c3b80f5b8b323f68a62d1f333375f7d124
96LawDawg <76912527+96LawDawg@users.noreply.github.com>
2023-11-05 21:29:31 -0600

    More syntax consistency.  This is what I get for copying what ChatGPT spits out.
    Not sure what is right or best.  But I did want to change as little as possible.

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -1502,27 +1502,26 @@ export class Widget extends StateManaged {
         }
       }
 
-      if(a.func === 'RECALL') {
+      if(a.func == 'RECALL') {
         setDefaults(a, { owned: true, inHolder: true, excludeCollection: null });
         let excludeCollection;
         if(this.isValidID(a.holder, problems)) {
           for(const holder of asArray(a.holder)) {
-            const decks = widgetFilter(w => w.get('type') === 'deck' && w.get('parent') === holder);
+            const decks = widgetFilter(w=>w.get('type')=='deck'&&w.get('parent')==holder);
             if(decks.length) {
-              let cards = [];
               for(const deck of decks) {
-                cards.push(...widgetFilter(w => w.get('deck') === deck.get('id')));
+                let cards = widgetFilter(w=>w.get('deck')==deck.get('id'));
               }
       
               if(!a.owned)
-                cards = cards.filter(c => !c.get('owner'));
+                cards = cards.filter(c=>!c.get('owner'));
               if(!a.inHolder)
-                cards = cards.filter(c => !c.get('_ancestor'));
+                cards = cards.filter(c=>!c.get('_ancestor'));
       
               if(a.excludeCollection !== null && (excludeCollection = getCollection(a.excludeCollection))) {
                 if(collections[excludeCollection] && collections[excludeCollection].length > 0) {
-                  const excludeCards = collections[excludeCollection].map(e => widgets.get(e.id));
-                  cards = cards.filter(c => !excludeCards.includes(c));
+                  const excludeCards = collections[excludeCollection].map(e=>widgets.get(e.id));
+                  cards = cards.filter(c=>!excludeCards.includes(c));
                 } else {
                   problems.push(`The collection ${a.excludeCollection} you want to exclude is empty or does not exist.`);
                 }
