c958e51ecbedfd0021fea4eb41ca59ce9c3f86bc
Mike <76912527+96LawDawg@users.noreply.github.com>
2023-11-16 08:30:39 -0600

    Add skipTurn for seats and TURN (#1942)

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -1834,7 +1834,7 @@ export class Widget extends StateManaged {
           a.turnCycle = 'forward'
         }
         let c = a.source === 'all' ? Array.from(widgets.values()) : collections[getCollection(a.source)] || [];
-        c = c.filter(w => w.get('type') === 'seat');
+        c = c.filter(w => w.get('type') === 'seat' && !w.get('skipTurn'));
 
         //this get the list of valid index
         const indexList = [];
