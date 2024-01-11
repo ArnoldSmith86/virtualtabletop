8c5c34a36f0799090fdf18b2be6238500e39585c
ArnoldSmith86 <arnold.smith.86@mail.ru>
2024-01-11 14:30:05 +0100

    missed two spots

--- client/js/domhelpers.js
+++ client/js/domhelpers.js
@@ -114,7 +114,7 @@ export function shuffleArray(array) {
   const isString = typeof array === 'string';
   array = [...array];
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(rand() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return isString ? array.join('') : array;
