660a5719c66ba7b2e2d2414a53f518c5acc2483a
Arnold Smith <arnold.smith.86@mail.ru>
2024-07-02 20:01:42 +0200

    implemented visibility toggle button

--- client/js/editor/toolbar/display.js
+++ client/js/editor/toolbar/display.js
@@ -1,11 +1,9 @@
 class ToggleDisplayButton extends PersistentToolbarToggleButton {
     constructor() {
       super('visibility', 'Toggle display of hidden widgets', 'Widgets that have "display":false will be alternately hidden and shown in the room.');
-      this.active = false;
     }
   
     toggle(state) {
-      selectionModeActive = state;
-      setSelection(selectedWidgets);
+      $('body').classList.toggle('showHiddenWidgets', state);
     }
   }
