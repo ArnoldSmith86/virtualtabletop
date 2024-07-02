a4b157f20b15fa9b6014a17b3aaeae7aeff00896
96LawDawg <76912527+96LawDawg@users.noreply.github.com>
2024-07-01 21:50:05 -0500

    Add visibility toggle button (does not do anything yet)

---
+++ client/js/editor/toolbar/display.js
@@ -0,0 +1,11 @@
+class ToggleDisplayButton extends PersistentToolbarToggleButton {
+    constructor() {
+      super('visibility', 'Toggle display of hidden widgets', 'Widgets that have "display":false will be alternately hidden and shown in the room.');
+      this.active = false;
+    }
+  
+    toggle(state) {
+      selectionModeActive = state;
+      setSelection(selectedWidgets);
+    }
+  }
