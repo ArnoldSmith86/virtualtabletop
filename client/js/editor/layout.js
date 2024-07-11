0b2344f8b9d82e05c62bef7a06dab154ea2afd74
Arnold Smith <arnold.smith.86@mail.ru>
2024-07-11 21:49:50 +0200

    swap move and resize buttons

--- client/js/editor/layout.js
+++ client/js/editor/layout.js
@@ -51,8 +51,8 @@ function initializeEditor(currentMetaData) {
     new CloneDragButton(),
     new SpacingDragButton(),
     new RotateDragButton(),
-    new MoveDragButton(),
-    new ResizeDragButton()
+    new ResizeDragButton(),
+    new MoveDragButton()
   ]);
 
   renderSidebar(sidebarModules = [
