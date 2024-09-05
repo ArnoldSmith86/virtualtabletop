760f04d448784778ed1c334160358d81c63ad41c
Arnold Smith <arnold.smith.86@mail.ru>
2024-09-05 17:51:20 +0200

    added Codespaces support

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -r "s/http:\/\/localhost:([0-9]+)/https:\/\/${CODESPACE_NAME}-\\1.app.github.dev/;s/VirtualTabletop.io/Codespaces VTT/;s/(allowPublicLibraryEdits.: )false/\\1true/" config.template.json > config.json
+npm install
