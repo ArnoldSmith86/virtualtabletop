37809041c338bff4890772802fe3021f8dcfc542
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2024-10-12 06:31:51 -0500

    Move/update volume controls

--- client/js/audio.js
+++ client/js/audio.js
@@ -1,9 +1,13 @@
 import { asArray, mapAssetURLs } from './domhelpers.js';
 
+let muted = false;
+let unmuteVol = 30;
+
 export let audioContext;
 const events = ['mousedown', 'keydown', 'touchstart'];
 let audioBufferObj = {}
 let sources = {};
+let audioSettings = {};
 
 events.forEach(event => {
   document.addEventListener(event, initializeAudioContext, { once: true });
@@ -35,7 +39,6 @@ async function loadAudioBuffer(audioSource) {
 }
 
 async function addAudio(audioSource, maxVolume, length) {
-  console.log('audioSource', audioSource, 'maxVolume', maxVolume, 'length', length)//fix
   if(audioContext) {
     let thisSource = audioContext.createBufferSource();
     if (!audioBufferObj[audioSource]) {
@@ -46,6 +49,7 @@ async function addAudio(audioSource, maxVolume, length) {
     gainNode.gain.value = Math.min(maxVolume * (((10 ** (document.getElementById('volume').value / 96.025)) / 10) - 0.1), 1); // converts slider to log scale with zero = no volume
     thisSource.connect(gainNode); // Connect the source to the gain node
     gainNode.connect(audioContext.destination); // Connect the gain node to the audioContext's destination
+    audioSettings[audioSource] = { gainNode, maxVolume };
 
     thisSource.buffer = audioBufferObj[mapAssetURLs(audioSource)];
 
@@ -60,7 +64,6 @@ async function addAudio(audioSource, maxVolume, length) {
 }
 
 onMessage('audio', function(args) {
-  console.log('audioArgs', args); //fix
   const { audioSource, maxVolume, length } = JSON.parse(args);
   try {
     addAudio(audioSource, maxVolume, length);
@@ -69,3 +72,46 @@ onMessage('audio', function(args) {
     console.log(err.message);
   }
 });
+
+on('#muteButton', 'click', function() {
+  if(muted) {
+    $('#volume').value = unmuteVol;
+    $('#muteButton').classList.remove('muted');
+    
+    // Update gain node using stored maxVolume
+    Object.keys(audioSettings).forEach(function(audioSource) {
+      const { gainNode, maxVolume } = audioSettings[audioSource];
+      gainNode.gain.value = Math.min(
+        maxVolume * (((10 ** (unmuteVol / 96.025)) / 10) - 0.1), 
+        1
+      );
+    });
+  } else {
+    unmuteVol = document.getElementById('volume').value;
+    $('#volume').value = 0;
+    
+    // Mute all gain nodes
+    Object.keys(audioSettings).forEach(function(audioSource) {
+      audioSettings[audioSource].gainNode.gain.value = 0;
+    });
+    
+    document.getElementById('muteButton').classList.add('muted');
+  }
+  muted = !muted;
+});
+
+if($('#volume')) {
+  on('#volume', 'input', function(){ // allows volume to be adjusted in real time
+    if(muted) {
+      $('#muteButton').classList.remove('muted');
+      muted = !muted;
+    }
+    Object.keys(audioSettings).forEach(function(audioSource) {
+      const { gainNode, maxVolume } = audioSettings[audioSource];
+      gainNode.gain.value = Math.min(
+        maxVolume * (((10 ** ($('#volume').value / 96.025)) / 10) - 0.1), 
+        1
+      );
+    });
+  });
+}
