261ae2029cd94431756a9ee11c5ebb0cdaacec9f
Arnold Smith <arnold.smith.86@mail.ru>
2025-05-24 19:39:54 +0200

    another one

--- server/room.mjs
+++ server/room.mjs
@@ -396,7 +396,7 @@ export default class Room {
   getPublicLibraryGames() {
     if(!Room.publicLibrary) {
       Room.publicLibrary = {};
-      for(const subLibrary of [ 'Games', 'Tutorials', 'Assets' ]) {
+      for(const subLibrary of [ 'Games', 'Tutorials' ]) {
         for(const dir of fs.readdirSync(Config.directory('library') + '/' + subLibrary.toLowerCase())) {
           const gameDir = Config.directory('library') + '/' + subLibrary.toLowerCase() + '/' + dir;
           if(fs.lstatSync(gameDir).isDirectory()) {
