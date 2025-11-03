3fe82bb76c53c66f289ed920180e50f6f7beb092
Brandon Lewis <brandon@mousewax.com>
2025-11-02 22:54:43 -0800

    refactor(widgets): Update card widgets to use regular HTML
    This commit refactors the card widgets to use regular HTML instead of iframes. It also fixes an infinite recursion bug and restores the card flipping logic.

--- client/js/widgets/card.js
+++ client/js/widgets/card.js
@@ -1,4 +1,4 @@
-class Card extends Widget {
+class Card extends BasicWidget {
   constructor(id) {
     super(id);
 
@@ -18,24 +18,21 @@ class Card extends Widget {
     this.deck = null;
   }
 
+  get(property) {
+    return Widget.prototype.get.call(this, property);
+  }
+
   applyDeltaToDOM(delta) {
     if(delta.deck !== undefined) {
-      const childNodes = [...this.domElement.childNodes];
       if(this.deck) {
-        this.domElement.innerHTML = '';
         this.deck.removeCard(this);
       }
       if(delta.deck) {
         this.deck = widgets.get(delta.deck);
         this.deck.addCard(this);
-        const faceTemplates = this.deck.get('faceTemplates');
-        this.createFaces(Array.isArray(faceTemplates) ? faceTemplates : []);
       } else {
         this.deck = null;
       }
-      for(const child of childNodes)
-        if(!child.className.match(/cardFace/))
-          this.domElement.appendChild(child);
     }
 
     if((delta.cardType !== undefined || delta.deck !== undefined) && this.deck) {
@@ -50,30 +47,9 @@ class Card extends Widget {
       this.applyDeltaToDOM(applyDefaultsFromDeck);
     }
 
-    if(delta.deck !== undefined || delta.activeFace !== undefined) {
-      for(let i=0; i<this.domElement.children.length; ++i) {
-        if(i == this.getActiveFace())
-          this.domElement.children[i].classList.add('active');
-        else
-          this.domElement.children[i].classList.remove('active');
-      }
-
-      const deltaForFaceChange = {};
-      if(this.previousFaceProperties)
-        for(const key in this.previousFaceProperties)
-          deltaForFaceChange[key] = this.get(key);
-      if(this.deck) {
-        this.previousFaceProperties = this.deck.getFaceProperties(this.getActiveFace());
-        for(const key in this.previousFaceProperties)
-          deltaForFaceChange[key] = this.get(key);
-      }
-      this.applyDeltaToDOM(deltaForFaceChange);
+    if (this.deck && (delta.deck !== undefined || delta.cardType !== undefined)) {
+        delta.faces = this.generateFaces();
     }
-
-    if(this.dynamicProperties)
-      for(const p in delta)
-        for(const callback of (this.dynamicProperties[p] || []))
-          callback();
 
     super.applyDeltaToDOM(delta);
   }
@@ -90,117 +66,6 @@ class Card extends Widget {
       throw `card type "${delta.cardType}" not found in deck "${delta.deck}"`;
   }
 
-  async click(mode='respect') {
-    if(!await super.click(mode))
-      await this.flip();
-  }
-
-  createFaces(faceTemplates) {
-    this.dynamicProperties = {};
-    for(const face of faceTemplates) {
-      const faceDiv = document.createElement('div');
-
-      faceDiv.classList.add('cardFace');
-      if(face.css !== undefined)
-        faceDiv.style.cssText = mapAssetURLs(this.cssAsText(face.css,null,true));
-      if(face.classes !== undefined)
-        faceDiv.classList.add(face.classes);
-      faceDiv.style.border = face.border ? face.border + 'px black solid' : 'none';
-      faceDiv.style.borderRadius = face.radius ? face.radius + 'px' : '0';
-
-      if(Array.isArray(face.objects)) {
-        for(const original of face.objects) {
-          const objectDiv = document.createElement(original.type == 'html' ? 'iframe' : 'div');
-          objectDiv.classList.add('cardFaceObject');
-
-          const setValue = _=>{
-            const usedProperties = new Set();
-            const object = JSON.parse(JSON.stringify(original));
-
-            if(typeof object.dynamicProperties == 'object')
-              for(const dp of Object.keys(object.dynamicProperties))
-                if(object[dp] === undefined)
-                  object[dp] = this.get(object.dynamicProperties[dp]);
-
-            const x = face.border ? object.x-face.border : object.x;
-            const y = face.border ? object.y-face.border : object.y;
-            let css = object.css ? this.cssAsText(object.css,usedProperties,true) + '; ' : '';
-            css += `left: ${x}px; top: ${y}px; width: ${object.width}px; height: ${object.height}px; font-size: ${object.fontSize}px; text-align: ${object.textAlign}`;
-            css += object.rotation ? `; transform: rotate(${object.rotation}deg)` : '';
-            if(typeof object.display !== 'undefined' && !object.display)
-              css += '; display: none';
-            objectDiv.style.cssText = mapAssetURLs(css);
-            if(object.classes)
-              objectDiv.classList.add(object.classes);
-
-            if(object.type == 'image') {
-              if(object.value) {
-                if(object.svgReplaces) {
-                  const replaces = { ...object.svgReplaces };
-                  for(const key in replaces)
-                    replaces[key] = this.get(replaces[key]);
-                  const svgResult = getSVG(object.value, replaces, _=>{
-                    objectDiv.style.backgroundImage = `url("${getSVG(object.value, replaces)}")`;
-                  });
-                  objectDiv.style.backgroundImage = `url("${svgResult}")`;
-                } else {
-                  objectDiv.style.backgroundImage = mapAssetURLs(`url("${object.value}")`);
-                }
-              }
-              objectDiv.style.backgroundColor = object.color || 'white';
-            } else if(object.type == 'icon') {
-              if(object.value) {
-                if($('.symbolOuterWrapper', objectDiv))
-                  $('.symbolOuterWrapper', objectDiv).remove();
-                generateSymbolsDiv(objectDiv, object.size || object.width, object.size || object.height, typeof object.value == 'object' ? object.value : Object.assign({ name:object.value }, object, { rotation: 0 }), object.text || '', 1, object.color);
-              }
-            } else if (object.type == 'html') {
-              // Prevent input from going to frame.
-              objectDiv.style.pointerEvents = 'none';
-              objectDiv.setAttribute('sandbox', 'allow-same-origin');
-              objectDiv.setAttribute('width', object.width);
-              objectDiv.setAttribute('height', object.height);
-              objectDiv.setAttribute('allow', 'autoplay');
-              const content = String(object.value).replaceAll(/\$\{PROPERTY ([A-Za-z0-9_-]+)\}/g, (m, n) => {
-                usedProperties.add(n);
-                return this.get(n) || '';
-              });
-              // Applies a template which fills available space, uses the same classes and applies
-              // nested CSS style rules.
-              const css = object['css'];
-              const extraStyles = typeof css == 'object' ? this.cssToStylesheet(css, usedProperties, true) : '';
-              const html = `<!DOCTYPE html>\n` +
-                  `<html><head><link rel="stylesheet" href="fonts.css"><style>html,body {height: 100%; margin: 0;} html {font-size: 14px; font-family: 'Roboto', sans-serif;} body {overflow: hidden;}${extraStyles}` +
-                  `</style></head><body class="${object.classes || ""}">${content}</body></html>`;
-              objectDiv.srcdoc = html;
-            } else {
-              objectDiv.textContent = object.value;
-              objectDiv.style.color = object.color;
-            }
-            return usedProperties;
-          }
-
-          // add a callback that makes sure dynamic property changes are reflected on the DOM
-          const properties = setValue();
-          if (original.svgReplaces)
-            for (const property of Object.values(original.svgReplaces))
-              properties.add(property);
-          if(typeof original.dynamicProperties == 'object')
-            for(const dp of Object.keys(original.dynamicProperties))
-              if(original[dp] === undefined)
-                properties.add(original.dynamicProperties[dp]);
-          for(const p of properties) {
-            if(!this.dynamicProperties[p])
-              this.dynamicProperties[p] = [];
-            this.dynamicProperties[p].push(setValue);
-          }
-
-          faceDiv.appendChild(objectDiv);
-        }
-      }
-      this.domElement.appendChild(faceDiv);
-    }
-  }
 
   cssProperties() {
     const p = super.cssProperties();
@@ -226,9 +91,95 @@ class Card extends Widget {
     return (face % count + count) % count;
   }
 
+  generateFaces() {
+    const faceTemplates = this.deck.get('faceTemplates');
+    if (!Array.isArray(faceTemplates)) {
+      return [];
+    }
+
+    const newFaces = [];
+    for (const face of faceTemplates) {
+      let faceHtml = '';
+      if (Array.isArray(face.objects)) {
+        for (const object of face.objects) {
+          const x = face.border ? object.x - face.border : object.x;
+          const y = face.border ? object.y - face.border : object.y;
+          
+          let css = `position: absolute; left: ${x}px; top: ${y}px; width: ${object.width}px; height: ${object.height}px; font-size: ${object.fontSize}px; text-align: ${object.textAlign}`;
+          if (object.rotation) {
+            css += `; transform: rotate(${object.rotation}deg)`;
+          }
+          if (typeof object.display !== 'undefined' && !object.display) {
+            css += '; display: none';
+          }
+          
+          let objectCss = '';
+           if (typeof object.css === 'string') {
+               objectCss = object.css;
+           } else if (typeof object.css === 'object' && object.css !== null) {
+               for (const [key, value] of Object.entries(object.css)) {
+                   if (typeof value !== 'object') {
+                       objectCss += `${key}: ${value};`;
+                   }
+               }
+           }
+           css += '; ' + objectCss;
+
+          let objectContent = '';
+          const objectClasses = `cardFaceObject ${object.classes || ''}`;
+          let objectHtml = '';
+
+          if (object.type === 'html') {
+            objectContent = object.value || '';
+            objectHtml = `<div class="${objectClasses}" style="${css}">${objectContent}</div>`;
+          } else if (object.type === 'text') {
+            objectContent = object.value || '';
+            css += `; color: ${object.color || 'black'};`;
+            objectHtml = `<div class="${objectClasses}" style="${css}">${objectContent}</div>`;
+          } else if (object.type === 'image') {
+            if (object.value) {
+              css += `; background-image: url('${object.value}');`;
+            }
+            css += `; background-color: ${object.color || 'white'};`;
+            objectHtml = `<div class="${objectClasses}" style="${css}"></div>`;
+          } else if (object.type === 'icon') {
+            objectHtml = `<!-- icon type not yet supported in HTML cards -->`;
+          }
+          
+          faceHtml += objectHtml;
+        }
+      }
+      
+      const faceDivClasses = `cardFace ${face.classes || ''}`;
+      let faceCss = '';
+      if (face.css) {
+        if (typeof face.css === 'string') {
+            faceCss = face.css;
+        } else if (typeof face.css === 'object' && face.css !== null) {
+            for (const [key, value] of Object.entries(face.css)) {
+                if (typeof value !== 'object') {
+                    faceCss += `${key}: ${value};`;
+                }
+            }
+        }
+      }
+      if (face.border) {
+        faceCss += `; border: ${face.border}px black solid;`;
+      }
+      if (face.radius) {
+        faceCss += `; border-radius: ${face.radius}px;`;
+      }
+
+      newFaces.push({
+        html: `<div class="${faceDivClasses}" style="${faceCss}">${faceHtml}</div>`
+      });
+    }
+    return newFaces;
+  }
+
   getDefaultValue(property) {
     if(this.deck && property != 'cardType' && property != 'activeFace') {
-      const d = this.deck.cardPropertyGet(this.get('cardType'), this.getActiveFace(), property);
+      const d = this.deck.cardPropertyGet(this.get('cardType'), this.get('activeFace'), property);
       if(d !== undefined)
         return d;
     }
