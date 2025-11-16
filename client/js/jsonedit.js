3684ae1cffae0882077fda137711c1e71c4be1c5
Arnold Smith <arnold.smith.86@mail.ru>
2025-11-16 07:31:01 +0100

    highlight HTML everywhere

--- client/js/jsonedit.js
+++ client/js/jsonedit.js
@@ -2230,6 +2230,27 @@ function jeColorize() {
   let out = [];
   let nr = 0;
   function push(line) {
+    // Highlight HTML tags and attributes
+    line = line.replace(/(&lt;\/?)([a-zA-Z][a-zA-Z0-9]*)(.*?)(&gt;)/g, (m, open, tag, attrs, close) => {
+      // Highlight tag name
+      let result = open + `<i class=htmltag>${tag}</i>`;
+      // Highlight attributes: attr="value" or attr='value'
+      attrs = attrs.replace(/(\s+)([a-zA-Z][a-zA-Z0-9-]*)(\s*=\s*)('|\\&quot;)([^&']*)(\4)/g, 
+        (m, ws, attr, eq, quote, value, quoteEnd) => {
+          if(attr === 'style') {
+            // Highlight CSS properties and values separately
+            value = value.replace(/([a-zA-Z-]+)(\s*:\s*)([^;]+)/g, 
+              (m, prop, colon, val) => `<i class=csskey>${prop}</i>${colon}<i class=cssvalue>${val.trim()}</i>`
+            );
+            return ws + `<i class=htmlattr>${attr}</i>${eq}${quote}${value}${quoteEnd}`;
+          }
+          return ws + `<i class=htmlattr>${attr}</i>${eq}${quote}<i class=htmlvalue>${value}</i>${quoteEnd}`;
+        }
+      );
+      result += attrs + close;
+      return result;
+    });
+
     out.push(`<div class=jeTextLine><span class=jeLineNumber>${nr}</span><span class=jeLineContent>${line}</span></div>`);
   }
   for(let line of jeGetEditorContent().split('\n')) {
@@ -2253,26 +2274,6 @@ function jeColorize() {
             match[i] = `<i class=${c[i]}>${html(match[i])}</i>`;
             if(l[i]=='string' || l[i]=='key') {
               match[i] = match[i].replace(/\$\{[^}]+\}/g, m=>`<i class=variable>${m}</i>`)
-              // Highlight HTML tags and attributes
-              match[i] = match[i].replace(/(&lt;\/?)([a-zA-Z][a-zA-Z0-9]*)(.*?)(&gt;)/g, (m, open, tag, attrs, close) => {
-                // Highlight tag name
-                let result = open + `<i class=htmltag>${tag}</i>`;
-                // Highlight attributes: attr="value" or attr='value'
-                attrs = attrs.replace(/(\s+)([a-zA-Z][a-zA-Z0-9-]*)(\s*=\s*)('|\\&quot;)([^&']*)(\4)/g, 
-                  (m, ws, attr, eq, quote, value, quoteEnd) => {
-                    if(attr === 'style') {
-                      // Highlight CSS properties and values separately
-                      value = value.replace(/([a-zA-Z-]+)(\s*:\s*)([^;]+)/g, 
-                        (m, prop, colon, val) => `<i class=csskey>${prop}</i>${colon}<i class=cssvalue>${val.trim()}</i>`
-                      );
-                      return ws + `<i class=htmlattr>${attr}</i>${eq}${quote}${value}${quoteEnd}`;
-                    }
-                    return ws + `<i class=htmlattr>${attr}</i>${eq}${quote}<i class=htmlvalue>${value}</i>${quoteEnd}`;
-                  }
-                );
-                result += attrs + close;
-                return result;
-              });
             }
           } else if(match[i])
             match[i] = html(match[i]);
