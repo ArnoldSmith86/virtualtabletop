5fee41048ef58e6e7af2918c8cb64d662cac3210
Roo <roo@example.com>
2025-11-20 12:52:17 -0800

    Refined combinations for guest player names.

--- client/js/generatednames.js
+++ client/js/generatednames.js
@@ -1,39 +1,17 @@
 import { rand } from './domhelpers.js';
 
 const adjectives = [
-  "Abstract", "Ameritrash", "Asymmetric", "Balanced", "Board", "Broken",
-  "Cardboard", "Casual", "Classic", "Clever", "Clunky", "Cooperative",
-  "Competitive", "Complex", "Crunchy", "Custom", "Damaged", "Deck-Building",
-  "Deduction", "Dexterity", "Digital", "Dry", "Elegant", "Epic", "Euro",
-  "Expansion", "Family", "Fast", "Fiddly", "First", "Foil", "Fun", "Game",
-  "Gateway", "Geeky", "Heavy", "Hidden", "Holo", "Immersive", "Indie",
-  "Legacy", "Legendary", "Light", "Long", "Losing", "Lucky", "Main",
-  "Mechanic", "Medium", "Meeple", "Miniature", "Mint", "Modern",
-  "Multiplayer", "Mythic", "New", "Old", "Original", "Painted", "Party",
-  "Plastic", "Played", "Player", "Print-and-Play", "Prototype", "Quick",
-  "Random", "Rare", "Rolled", "Rule-Abiding", "Serious", "Short",
-  "Shuffled", "Simple", "Single", "Sleeved", "Social", "Solo", "Standard",
-  "Strategic", "Symmetric", "Tabletop", "Tactical", "Thematic", "Tied",
-  "Tile-Laying", "Turn-Based", "Unbalanced", "Uncommon", "Unpainted",
-  "Unsleeved", "Vintage", "Wargame", "Winning", "Wooden", "Worker-Placement"
+  "Wobbly", "Tiny", "Sparkly", "Squishy", "Chunky", "Bendy",
+  "Shiny", "Silly", "Goofy", "Clumsy", "Zany", "Quirky",
+  "Wonky", "Bouncy", "Puffy", "Jiggly", "Fluffy", "Funky",
+  "Cheery", "Dizzy", "Wiggly", "Noodly", "Plump", "Snappy",
+  "Dorky", "Peppy", "Perky", "Loopy", "Zippy", "Bubbly"
 ];
 
 const nouns = [
-  "Ability", "Action", "Area", "Attack", "Auction", "Bag", "Binder",
-  "Board", "Box", "Brick", "Card", "Chair", "Chase", "Chit", "Coin",
-  "Collection", "Combo", "Component", "Control", "Counter", "Cube",
-  "Cylinder", "Damage", "Deck", "Deduction", "Defeat", "Defense",
-  "Designer", "Dexterity", "Die", "Dice", "Discard", "Disk", "Draft",
-  "Effect", "Engine", "Expansion", "Figurine", "Game", "Gamer", "Gold",
-  "Hand", "Health", "Hex", "Insert", "Instruction", "Leader", "Location",
-  "Loser", "Mana", "Map", "Marker", "Mat", "Match", "Meeple", "Mini",
-  "Miniature", "Money", "Move", "Opponent", "Ore", "Organizer", "Overlay",
-  "Pack", "Pawn", "Phase", "Piece", "Placement", "Player", "Point",
-  "Prototype", "Publisher", "Pyramid", "Race", "Resource", "Round",
-  "Rule", "Rulebook", "Score", "Set", "Setup", "Sheep", "Shelf", "Skill",
-  "Sleeve", "Standee", "Stone", "Strategy", "Table", "Tactic", "Teammate",
-  "Tile", "Timer", "Token", "Track", "Trigger", "Turn", "Variant",
-  "Victory", "Wheat", "Winner", "Wood", "Worker"
+  "Board", "Box", "Bag", "Card", "Coin", "Cube",
+  "Die", "Disk", "Hex", "Map", "Mat", "Pawn",
+  "Piece", "Tile", "Token"
 ];
 
 export function generateName() {
