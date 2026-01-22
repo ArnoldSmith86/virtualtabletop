07317ee8e89928c396118daa4dc202f35c6a7c7e
Arnold Smith <arnold.smith.86@mail.ru>
2026-01-22 11:08:10 +0100

    make sure we got a good asset filename

--- client/js/widgets/widget.js
+++ client/js/widgets/widget.js
@@ -2053,7 +2053,7 @@ export class Widget extends StateManaged {
       if(a.func == 'UPLOAD') {
         setDefaults(a, { variable: 'uploadedFileName', fileTypes: null });
         const uploadedAsset = await uploadAsset(null, a.fileTypes);
-        if(uploadedAsset === null) {
+        if(!String(uploadedAsset).match(/^\/assets\/[0-9_-]+$/)) {
           variables[a.variable] = false;
           if(jeRoutineLogging)
             jeLoggingRoutineOperationSummary("UPLOAD cancelled");
